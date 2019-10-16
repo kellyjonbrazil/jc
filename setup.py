caf480c63aacb518805d48fba0fd454959d11023
Kelly Brazil <kellyjonbrazil@gmail.com>
2019-10-16 16:01:34 -0700

    module fix

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     entry_points={
         'console_scripts': [
-            'jc=jc.py:main',
+            'jc=jc:main',
         ],
     },
     classifiers=[
