4f552e370e656cc1882a7dae231475a016f8aa14
Kelly Brazil <kellyjonbrazil@gmail.com>
2019-10-18 13:26:24 -0700

    acknowledgment

--- jc/parsers/ps.py
+++ jc/parsers/ps.py
@@ -14,8 +14,10 @@ Examples:
 
 def parse(data):
 
-    cleandata = data.splitlines()
+    # code adapted from Conor Heine at:
+    # https://gist.github.com/cahna/43a1a3ff4d075bcd71f9d7120037a501
 
+    cleandata = data.splitlines()
     headers = [h for h in ' '.join(cleandata[0].strip().split()).split() if h]
     raw_data = map(lambda s: s.strip().split(None, len(headers) - 1), cleandata[1:])
     return [dict(zip(headers, r)) for r in raw_data]
