2eba30422b44f648c7356a44cbff7416c6648f19
Kelly Brazil <kellyjonbrazil@gmail.com>
2019-10-18 13:43:49 -0700

    skip first line

--- jc/parsers/route.py
+++ jc/parsers/route.py
@@ -15,7 +15,7 @@ def parse(data):
     # code adapted from Conor Heine at:
     # https://gist.github.com/cahna/43a1a3ff4d075bcd71f9d7120037a501
 
-    cleandata = data.splitlines()
+    cleandata = data.splitlines()[1:]
     headers = [h for h in ' '.join(cleandata[0].strip().split()).split() if h]
     raw_data = map(lambda s: s.strip().split(None, len(headers) - 1), cleandata[1:])
     return [dict(zip(headers, r)) for r in raw_data]
