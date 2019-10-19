a9294f32a00709737d0b82ff434a311e75ad0bed
Kelly Brazil <kellyjonbrazil@gmail.com>
2019-10-18 18:40:56 -0700

    ls fixes

--- jc/parsers/ls.py
+++ jc/parsers/ls.py
@@ -89,43 +89,43 @@ import re
 def parse(data):
     output = []
 
-    cleandata = data.splitlines()
+    linedata = data.splitlines()
 
     # Delete first line if it starts with 'total'
-    if cleandata[0].find('total') == 0:
-        cleandata.pop(0)
+    if linedata[0].find('total') == 0:
+        linedata.pop(0)
 
-    # Delete last line if it is blank
-    if cleandata[-1] == '':
-        cleandata.pop(-1)
+    # Clear any blank lines
+    cleandata = list(filter(None, linedata))
 
-    # Check if -l was used to parse extra data
-    if re.match('^[-dclpsbDCMnP?]([-r][-w][-xsS]){2}([-r][-w][-xtT])[+]?', cleandata[0]):
-        for entry in cleandata:
-            output_line = {}
+    if cleandata:
+        # Check if -l was used to parse extra data
+        if re.match('^[-dclpsbDCMnP?]([-r][-w][-xsS]){2}([-r][-w][-xtT])[+]?', cleandata[0]):
+            for entry in cleandata:
+                output_line = {}
 
-            parsed_line = entry.split()
+                parsed_line = entry.split(maxsplit=8)
 
-            # split filenames and links
-            filename_field = ' '.join(parsed_line[8:]).split(' -> ')
+                # split filenames and links
+                filename_field = parsed_line[8].split(' -> ')
 
-            # create list of dictionaries
-            output_line['filename'] = filename_field[0]
+                # create list of dictionaries
+                output_line['filename'] = filename_field[0]
 
-            if len(filename_field) > 1:
-                output_line['link_to'] = filename_field[1]
+                if len(filename_field) > 1:
+                    output_line['link_to'] = filename_field[1]
 
-            output_line['flags'] = parsed_line[0]
-            output_line['links'] = int(parsed_line[1])
-            output_line['owner'] = parsed_line[2]
-            output_line['group'] = parsed_line[3]
-            output_line['bytes'] = int(parsed_line[4])
-            output_line['date'] = ' '.join(parsed_line[5:8])
-            output.append(output_line)
-    else:
-        for entry in cleandata:
-            output_line = {}
-            output_line['filename'] = entry
-            output.append(output_line)
+                output_line['flags'] = parsed_line[0]
+                output_line['links'] = int(parsed_line[1])
+                output_line['owner'] = parsed_line[2]
+                output_line['group'] = parsed_line[3]
+                output_line['bytes'] = int(parsed_line[4])
+                output_line['date'] = ' '.join(parsed_line[5:8])
+                output.append(output_line)
+        else:
+            for entry in cleandata:
+                output_line = {}
+                output_line['filename'] = entry
+                output.append(output_line)
 
     return output
