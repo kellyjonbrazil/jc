e3750b49628794fd4d563013b1f3a7471c27ce68
Kelly Brazil <kellyjonbrazil@gmail.com>
2019-10-22 07:40:42 -0700

    documentation enhancements

--- README.md
+++ README.md
@@ -18,12 +18,32 @@ $ ls -l /usr/bin | jc --ls | jq '.[] | select(.bytes > 50000000)'
 }
 ```
 
-The `jc` parsers can also be used as python modules by referencing them via:
+The `jc` parsers can also be used as python modules:
 ```
-import jc.parsers.[parser]
-
-data = 'data to parse'
-jc.parsers.[parser].parse(data)
+>>> import jc.parsers.ls
+>>>
+>>> data='''-rwxr-xr-x  1 root  wheel    23648 May  3 22:26 cat
+... -rwxr-xr-x  1 root  wheel    30016 May  3 22:26 chmod
+... -rwxr-xr-x  1 root  wheel    29024 May  3 22:26 cp
+... -rwxr-xr-x  1 root  wheel   375824 May  3 22:26 csh
+... -rwxr-xr-x  1 root  wheel    28608 May  3 22:26 date
+... -rwxr-xr-x  1 root  wheel    32000 May  3 22:26 dd
+... -rwxr-xr-x  1 root  wheel    23392 May  3 22:26 df
+... -rwxr-xr-x  1 root  wheel    18128 May  3 22:26 echo'''
+>>>
+>>> jc.parsers.ls.parse(data)
+[{'filename': 'cat', 'flags': '-rwxr-xr-x', 'links': 1, 'owner': 'root', 'group': 'wheel', 
+'bytes': 23648, 'date': 'May 3 22:26'}, {'filename': 'chmod', 'flags': '-rwxr-xr-x', 'links': 1, 
+'owner': 'root', 'group': 'wheel', 'bytes': 30016, 'date': 'May 3 22:26'}, {'filename': 'cp', 
+'flags': '-rwxr-xr-x', 'links': 1, 'owner': 'root', 'group': 'wheel', 'bytes': 29024, 
+'date': 'May 3 22:26'}, {'filename': 'csh', 'flags': '-rwxr-xr-x', 'links': 1, 'owner': 'root', 
+'group': 'wheel', 'bytes': 375824, 'date': 'May 3 22:26'}, {'filename': 'date', 
+'flags': '-rwxr-xr-x', 'links': 1, 'owner': 'root', 'group': 'wheel', 'bytes': 28608, 
+'date': 'May 3 22:26'}, {'filename': 'dd', 'flags': '-rwxr-xr-x', 'links': 1, 'owner': 'root', 
+'group': 'wheel', 'bytes': 32000, 'date': 'May 3 22:26'}, {'filename': 'df', 'flags': '-rwxr-xr-x', 
+'links': 1, 'owner': 'root', 'group': 'wheel', 'bytes': 23392, 'date': 'May 3 22:26'}, 
+{'filename': 'echo', 'flags': '-rwxr-xr-x', 'links': 1, 'owner': 'root', 'group': 'wheel', 
+'bytes': 18128, 'date': 'May 3 22:26'}]
 ```
 In this case the output will be a python dictionary instead of JSON.
 
--- jc/__init__.py
+++ jc/__init__.py
@@ -5,55 +5,67 @@
 This module serializes standard unix command line output to structured JSON
 output.
 
-Example:
+CLI Example:
 
-$ ls -al | jc | jq .
+$ ls -l /bin | jc --ls -p
 [
-    {
-        "filename": ".",
-        "suffix": Null,
-        "bytes": 224,
-        "date_updated": "Oct  1 12:09",
-        "owner_user": "joeuser",
-        "owner_group": "staff",
-        "flags": "drwxr-xr-x+",
-        "link_to": Null,
-        "links": 47
-    },
-    {
-        "filename": "..",
-        "suffix": Null,
-        "bytes": 224,
-        "date_updated": "Oct  1 12:09",
-        "owner_user": "admin",
-        "owner_group": "root",
-        "flags": "drwxr-xr-x",
-        "link_to": Null,
-        "links": 7
-    },
-    {
-        "filename": "testfile.txt",
-        "suffix": "txt",
-        "bytes": 14686,
-        "date_updated": "Oct  1 12:09",
-        "owner_user": "joeuser",
-        "owner_group": "staff",
-        "flags": "-rwxr-xr-x@",
-        "link_to": Null,
-        "links": 1
-    },
-    {
-        "filename": "ncat",
-        "suffix": Null,
-        "bytes": 14686,
-        "date_updated": "Oct  1 12:09",
-        "owner_user": "joeuser",
-        "owner_group": "staff",
-        "flags": "lrwxr-xr-x",
-        "link_to": "../Cellar/nmap/7.70/bin/ncat",
-        "links": 1
-    }
+  {
+    "filename": "bash",
+    "flags": "-r-xr-xr-x",
+    "links": 1,
+    "owner": "root",
+    "group": "wheel",
+    "bytes": 618416,
+    "date": "May 3 22:26"
+  },
+  {
+    "filename": "cat",
+    "flags": "-rwxr-xr-x",
+    "links": 1,
+    "owner": "root",
+    "group": "wheel",
+    "bytes": 23648,
+    "date": "May 3 22:26"
+  },
+  {
+    "filename": "chmod",
+    "flags": "-rwxr-xr-x",
+    "links": 1,
+    "owner": "root",
+    "group": "wheel",
+    "bytes": 30016,
+    "date": "May 3 22:26"
+  },
+  ...
 ]
+
+
+Module Example:
+
+>>> import jc.parsers.ls
+>>>
+>>> data='''-rwxr-xr-x  1 root  wheel    23648 May  3 22:26 cat
+... -rwxr-xr-x  1 root  wheel    30016 May  3 22:26 chmod
+... -rwxr-xr-x  1 root  wheel    29024 May  3 22:26 cp
+... -rwxr-xr-x  1 root  wheel   375824 May  3 22:26 csh
+... -rwxr-xr-x  1 root  wheel    28608 May  3 22:26 date
+... -rwxr-xr-x  1 root  wheel    32000 May  3 22:26 dd
+... -rwxr-xr-x  1 root  wheel    23392 May  3 22:26 df
+... -rwxr-xr-x  1 root  wheel    18128 May  3 22:26 echo'''
+>>>
+>>> jc.parsers.ls.parse(data)
+[{'filename': 'cat', 'flags': '-rwxr-xr-x', 'links': 1, 'owner': 'root', 'group': 'wheel', 
+'bytes': 23648, 'date': 'May 3 22:26'}, {'filename': 'chmod', 'flags': '-rwxr-xr-x', 'links': 1, 
+'owner': 'root', 'group': 'wheel', 'bytes': 30016, 'date': 'May 3 22:26'}, {'filename': 'cp', 
+'flags': '-rwxr-xr-x', 'links': 1, 'owner': 'root', 'group': 'wheel', 'bytes': 29024, 
+'date': 'May 3 22:26'}, {'filename': 'csh', 'flags': '-rwxr-xr-x', 'links': 1, 'owner': 'root', 
+'group': 'wheel', 'bytes': 375824, 'date': 'May 3 22:26'}, {'filename': 'date', 
+'flags': '-rwxr-xr-x', 'links': 1, 'owner': 'root', 'group': 'wheel', 'bytes': 28608, 
+'date': 'May 3 22:26'}, {'filename': 'dd', 'flags': '-rwxr-xr-x', 'links': 1, 'owner': 'root', 
+'group': 'wheel', 'bytes': 32000, 'date': 'May 3 22:26'}, {'filename': 'df', 'flags': '-rwxr-xr-x', 
+'links': 1, 'owner': 'root', 'group': 'wheel', 'bytes': 23392, 'date': 'May 3 22:26'}, 
+{'filename': 'echo', 'flags': '-rwxr-xr-x', 'links': 1, 'owner': 'root', 'group': 'wheel', 
+'bytes': 18128, 'date': 'May 3 22:26'}]
 """
 
 name = 'jc'
