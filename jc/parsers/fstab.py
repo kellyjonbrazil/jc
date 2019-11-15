9b453bcb84dd9f2f566955eda1ab35c863b3e8a1
Kelly Brazil <kellyjonbrazil@gmail.com>
2019-11-15 09:57:25 -0800

    remove commented code block

--- jc/parsers/fstab.py
+++ jc/parsers/fstab.py
@@ -106,25 +106,13 @@ def parse(data, raw=False, quiet=False):
             if line.strip().find('#') == 0:
                 continue
 
-            line_list = line.split(maxsplit=5)
+            line_list = line.split(maxsplit=6)
             fs_spec = line_list[0]
             fs_file = line_list[1]
             fs_vfstype = line_list[2]
             fs_mntops = line_list[3]
             fs_freq = line_list[4]
             fs_passno = line_list[5]
-
-            # fstab_list = fstab.split()
-
-            # comment_found = False
-            # for i, item in enumerate(fstab_list):
-            #     if item.find('#') != -1:
-            #         comment_found = True
-            #         comment_item = i
-            #         break
-
-            # if comment_found:
-            #     fstab_list = fstab_list[:comment_item]
 
             output_line['fs_spec'] = fs_spec
             output_line['fs_file'] = fs_file
