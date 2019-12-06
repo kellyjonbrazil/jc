46b827da6b438be73fab168c3f05298483b141e4
Kelly Brazil <kellyjonbrazil@gmail.com>
2019-12-06 13:22:51 -0800

    add osx compatibility

--- jc/parsers/arp.py
+++ jc/parsers/arp.py
@@ -128,7 +128,7 @@ def parse(data, raw=False, quiet=False):
     """
 
     # compatible options: linux, darwin, cygwin, win32, aix, freebsd
-    compatible = ['linux', 'aix', 'freebsd']
+    compatible = ['linux', 'aix', 'freebsd', 'darwin']
 
     if not quiet:
         jc.utils.compatibility(__name__, compatible)
@@ -139,11 +139,29 @@ def parse(data, raw=False, quiet=False):
     cleandata = data.splitlines()
 
     # remove final Entries row if -v was used
-    if cleandata[-1].find("Entries:") == 0:
+    if cleandata[-1].find('Entries:') == 0:
         cleandata.pop(-1)
 
-    # detect if linux or bsd style was used
-    if cleandata[0].find('Address') == 0:
+    # detect if osx style was used
+    if cleandata[0].find(' ifscope ') != -1:
+        raw_output = []
+        for line in cleandata:
+            line = line.split()
+            output_line = {}
+            output_line['name'] = line[0]
+            output_line['address'] = line[1].lstrip('(').rstrip(')')
+            output_line['hwtype'] = line[-1].lstrip('[').rstrip(']')
+            output_line['hwaddress'] = line[3]
+            output_line['iface'] = line[5]
+            raw_output.append(output_line)
+
+        if raw:
+            return raw_output
+        else:
+            return process(raw_output)
+
+    # detect if linux style was used
+    elif cleandata[0].find('Address') == 0:
 
         # fix header row to change Flags Mask to flags_mask
         cleandata[0] = cleandata[0].replace('Flags Mask', 'flags_mask')
@@ -157,6 +175,7 @@ def parse(data, raw=False, quiet=False):
         else:
             return process(raw_output)
 
+    # otherwise, try bsd style
     else:
         raw_output = []
         for line in cleandata:
