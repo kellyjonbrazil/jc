5f798d603e461d2de5adf74662ba4d4b121f28f2
Kelly Brazil <kellyjonbrazil@gmail.com>
2020-03-04 16:11:14 -0800

    version bump and ack to philippeitis

--- jc/parsers/history.py
+++ jc/parsers/history.py
@@ -44,10 +44,11 @@ import jc
 
 
 class info():
-    version = '1.1'
+    version = '1.2'
     description = 'history command parser'
     author = 'Kelly Brazil'
     author_email = 'kellyjonbrazil@gmail.com'
+    details = 'Optimizations by https://github.com/philippeitis'
 
     # compatible options: linux, darwin, cygwin, win32, aix, freebsd
     compatible = ['linux', 'darwin', 'cygwin', 'aix', 'freebsd']
