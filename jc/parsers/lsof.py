548aaab6262878d21f8b9539950cfad48638e35c
Kelly Brazil <kellyjonbrazil@gmail.com>
2020-04-04 17:04:20 -0700

    remove old commented code

--- jc/parsers/lsof.py
+++ jc/parsers/lsof.py
@@ -97,7 +97,7 @@ import jc.parsers.universal
 
 
 class info():
-    version = '1.0'
+    version = '1.1'
     description = 'lsof command parser'
     author = 'Kelly Brazil'
     author_email = 'kellyjonbrazil@gmail.com'
@@ -179,47 +179,6 @@ def parse(data, raw=False, quiet=False):
         cleandata[0] = cleandata[0].replace('/', '_')
 
         raw_output = jc.parsers.universal.sparse_table_parse(cleandata)
-
-        '''
-        # find column value of last character of each header
-        header_text = cleandata.pop(0).lower()
-
-        # clean up 'size/off' header
-        # even though forward slash in a key is valid json, it can make things difficult
-        header_row = header_text.replace('/', '_')
-
-        headers = header_row.split()
-
-        header_spec = []
-        for i, h in enumerate(headers):
-            # header tuple is (index, header_name, col)
-            header_spec.append((i, h, header_row.find(h) + len(h)))
-
-        # parse lines
-        for entry in cleandata:
-            output_line = {}
-
-            # normalize data by inserting Null for missing data
-            temp_line = entry.split(maxsplit=len(headers) - 1)
-
-            for spec in header_spec:
-
-                index = spec[0]
-                header_name = spec[1]
-                col = spec[2] - 1     # subtract one since column starts at 0 instead of 1
-
-                if header_name == 'command' or header_name == 'name':
-                    continue
-                if entry[col] in string.whitespace:
-                    temp_line.insert(index, None)
-
-            name = ' '.join(temp_line[9:])
-            fixed_line = temp_line[0:9]
-            fixed_line.append(name)
-
-            output_line = dict(zip(headers, fixed_line))
-            raw_output.append(output_line)
-        '''
 
     if raw:
         return raw_output
