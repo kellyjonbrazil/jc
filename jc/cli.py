b7756d92501a7b8ba8e77d77d9d3718a58425beb
Kelly Brazil <kellyjonbrazil@gmail.com>
2020-04-20 16:33:26 -0700

    version bump

--- jc/cli.py
+++ jc/cli.py
@@ -18,7 +18,7 @@ import jc.utils
 
 
 class info():
-    version = '1.10.5'
+    version = '1.10.6'
     description = 'jc cli output JSON conversion tool'
     author = 'Kelly Brazil'
     author_email = 'kellyjonbrazil@gmail.com'
