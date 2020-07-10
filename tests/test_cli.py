28ed17ad3bce866e5dc0179ef5de43380ac6ace1
Kelly Brazil <kellyjonbrazil@gmail.com>
2020-07-10 15:45:21 -0700

    add parser_count test to test_cli_about_jc

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -176,3 +176,4 @@ class MyTests(unittest.TestCase):
 
     def test_cli_about_jc(self):
         self.assertEqual(jc.cli.about_jc()['name'], 'jc')
+        self.assertGreaterEqual(jc.cli.about_jc()['parser_count'], 50)
