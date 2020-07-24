928e39cd103b96b8c3ccc8d85c930ffb419296c6
Kelly Brazil <kellyjonbrazil@gmail.com>
2020-07-24 14:16:41 -0700

    add generic traceroute tests

--- tests/test_traceroute.py
+++ tests/test_traceroute.py
@@ -34,6 +34,30 @@ class MyTests(unittest.TestCase):
         with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/osx-10.14.6/traceroute6.out'), 'r', encoding='utf-8') as f:
             self.osx_10_14_6_traceroute6 = f.read()
 
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/traceroute1.out'), 'r', encoding='utf-8') as f:
+            self.generic_traceroute1 = f.read()
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/traceroute2.out'), 'r', encoding='utf-8') as f:
+            self.generic_traceroute2 = f.read()
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/traceroute3.out'), 'r', encoding='utf-8') as f:
+            self.generic_traceroute3 = f.read()
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/traceroute4.out'), 'r', encoding='utf-8') as f:
+            self.generic_traceroute4 = f.read()
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/traceroute5.out'), 'r', encoding='utf-8') as f:
+            self.generic_traceroute5 = f.read()
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/traceroute6.out'), 'r', encoding='utf-8') as f:
+            self.generic_traceroute6 = f.read()
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/traceroute7.out'), 'r', encoding='utf-8') as f:
+            self.generic_traceroute7 = f.read()
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/traceroute8.out'), 'r', encoding='utf-8') as f:
+            self.generic_traceroute8 = f.read()
+
         # output
         with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/centos-7.7/traceroute.json'), 'r', encoding='utf-8') as f:
             self.centos_7_7_traceroute_json = json.loads(f.read())
@@ -55,6 +79,30 @@ class MyTests(unittest.TestCase):
 
         with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/osx-10.14.6/traceroute6.json'), 'r', encoding='utf-8') as f:
             self.osx_10_14_6_traceroute6_json = json.loads(f.read())
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/traceroute1.json'), 'r', encoding='utf-8') as f:
+            self.generic_traceroute1_json = json.loads(f.read())
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/traceroute2.json'), 'r', encoding='utf-8') as f:
+            self.generic_traceroute2_json = json.loads(f.read())
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/traceroute3.json'), 'r', encoding='utf-8') as f:
+            self.generic_traceroute3_json = json.loads(f.read())
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/traceroute4.json'), 'r', encoding='utf-8') as f:
+            self.generic_traceroute4_json = json.loads(f.read())
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/traceroute5.json'), 'r', encoding='utf-8') as f:
+            self.generic_traceroute5_json = json.loads(f.read())
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/traceroute6.json'), 'r', encoding='utf-8') as f:
+            self.generic_traceroute6_json = json.loads(f.read())
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/traceroute7.json'), 'r', encoding='utf-8') as f:
+            self.generic_traceroute7_json = json.loads(f.read())
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/traceroute8.json'), 'r', encoding='utf-8') as f:
+            self.generic_traceroute8_json = json.loads(f.read())
 
     def test_traceroute_nodata(self):
         """
@@ -109,6 +157,54 @@ class MyTests(unittest.TestCase):
         Test 'traceroute6' on OSX 10.14.6
         """
         self.assertEqual(jc.parsers.traceroute.parse(self.osx_10_14_6_traceroute6, quiet=True), self.osx_10_14_6_traceroute6_json)
+
+    def test_traceroute1_generic(self):
+        """
+        Test 'traceroute'
+        """
+        self.assertEqual(jc.parsers.traceroute.parse(self.generic_traceroute1, quiet=True), self.generic_traceroute1_json)
+
+    def test_traceroute2_generic(self):
+        """
+        Test 'traceroute'
+        """
+        self.assertEqual(jc.parsers.traceroute.parse(self.generic_traceroute2, quiet=True), self.generic_traceroute2_json)
+
+    def test_traceroute3_generic(self):
+        """
+        Test 'traceroute'
+        """
+        self.assertEqual(jc.parsers.traceroute.parse(self.generic_traceroute3, quiet=True), self.generic_traceroute3_json)
+
+    def test_traceroute4_generic(self):
+        """
+        Test 'traceroute'
+        """
+        self.assertEqual(jc.parsers.traceroute.parse(self.generic_traceroute4, quiet=True), self.generic_traceroute4_json)
+
+    def test_traceroute5_generic(self):
+        """
+        Test 'traceroute'
+        """
+        self.assertEqual(jc.parsers.traceroute.parse(self.generic_traceroute5, quiet=True), self.generic_traceroute5_json)
+
+    def test_traceroute6_generic(self):
+        """
+        Test 'traceroute'
+        """
+        self.assertEqual(jc.parsers.traceroute.parse(self.generic_traceroute6, quiet=True), self.generic_traceroute6_json)
+
+    def test_traceroute7_generic(self):
+        """
+        Test 'traceroute'
+        """
+        self.assertEqual(jc.parsers.traceroute.parse(self.generic_traceroute7, quiet=True), self.generic_traceroute7_json)
+
+    def test_traceroute8_generic(self):
+        """
+        Test 'traceroute'
+        """
+        self.assertEqual(jc.parsers.traceroute.parse(self.generic_traceroute8, quiet=True), self.generic_traceroute8_json)
 
 
 if __name__ == '__main__':
