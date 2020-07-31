d3be61f60837801d5a505dc2520cc80c874a6092
Kelly Brazil <kellyjonbrazil@gmail.com>
2020-07-31 10:59:46 -0700

    version bump

--- jc/parsers/traceroute.py
+++ jc/parsers/traceroute.py
@@ -92,7 +92,7 @@ import jc.utils
 
 
 class info():
-    version = '1.0'
+    version = '1.1'
     description = 'traceroute command parser'
     author = 'Kelly Brazil'
     author_email = 'kellyjonbrazil@gmail.com'
