f9921720cd0b9b0d76d66e6eb5bfe43481f5f52b
Kelly Brazil <kellyjonbrazil@gmail.com>
2021-03-22 22:01:07 -0700

    revert to local testing for naive datetime objects

--- runtests.sh
+++ runtests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-TZ="America/Los_Angeles" python3 -m unittest -v
+python3 -m unittest -v
 
 echo
-echo "Running local-only tests:"
+echo "##################### Running local-only tests: #####################"
 echo
 
-# python3 -m unittest tests.localtest_date -v
-# python3 -m unittest tests.localtest_last -v
+python3 -m unittest tests.localtest_date -v
+python3 -m unittest tests.localtest_last -v
