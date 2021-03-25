e9febe98ac5ec0e03791c5b6794446a2c3312fb1
Kelly Brazil <kellyjonbrazil@gmail.com>
2021-03-25 11:17:01 -0700

    add localtest partition lines for readability

--- runtests.sh
+++ runtests.sh
@@ -7,5 +7,7 @@ echo "##################### Running local-only tests: #####################"
 echo
 
 python3 -m unittest tests.localtest_date -v
+echo -e "\n======================================================================\n"
 python3 -m unittest tests.localtest_last -v
+echo -e "\n======================================================================\n"
 python3 -m unittest tests.localtest_upower -v
