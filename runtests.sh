c1b0d27752726989c6051f683a1194941bee598c
Kelly Brazil <kellyjonbrazil@gmail.com>
2021-03-25 20:45:38 -0700

    remove old commented commands

--- runtests.sh
+++ runtests.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
 python3 -m unittest -v
-
-# echo
-# echo "##################### Running local-only tests: #####################"
-# echo
-
-# python3 -m unittest tests.localtest_date -v
-# echo -e "\n======================================================================\n"
-# python3 -m unittest tests.localtest_last -v
-# echo -e "\n======================================================================\n"
-# python3 -m unittest tests.localtest_upower -v
