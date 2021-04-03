09c1fccc584e3d0582e19b520586419085ea8d04
Kelly Brazil <kellyjonbrazil@gmail.com>
2021-04-02 18:04:55 -0700

    manpage updates

--- mangen.py
+++ mangen.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # Genereate man page from jc metadata using jinja2 templates
 from datetime import date
-import jc
 import jc.cli
 from jinja2 import Environment, FileSystemLoader
 
@@ -11,9 +10,8 @@ template = env.get_template('manpage_template')
 
 # get parser info from jc.cli.about_jc()
 # plug it into the man page jinja2 template
-jcinfo = jc.cli.about_jc()
-today = date.today()
-output = template.render(today=today, jc=jcinfo)
+output = template.render(today=date.today(),
+                         jc=jc.cli.about_jc())
 
 # save to man/jc.1
 with open('man/jc.1', 'w') as f:
