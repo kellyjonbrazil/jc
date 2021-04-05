88bd7554aeb511c19e55dc3dde9c9376efb0485b
Kelly Brazil <kellyjonbrazil@gmail.com>
2021-04-05 09:42:25 -0700

    add status messages

--- docgen.sh
+++ docgen.sh
@@ -3,7 +3,9 @@
 # requires pydoc-markdown 2.1.0.post1
 
 cd jc
+echo Building docs for: package
 pydocmd simple jc+ > ../docs/readme.md
+echo Building docs for: utils
 pydocmd simple utils+ > ../docs/utils.md
 
 # a bit of inception here... jc is being used to help
--- updatedocs.sh
+++ updatedocs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Update all documentation (README.md, Man page, Doc files)
 
-./readmegen.py
-./mangen.py
-./docgen.sh
+echo === Building README.md
+./readmegen.py && echo "+++ README.md build successful" || echo "--- README.md build failed"
+echo === Building man page
+./mangen.py && echo "+++ man page build successful" || echo "--- man page build failed"
+echo === Building documentation
+./docgen.sh && echo "+++ documentation build successful" || echo "--- documentation build failed"
