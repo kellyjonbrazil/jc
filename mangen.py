b5ec16c5cafb78db093861cc30d9646207f2227a
Kelly Brazil <kellyjonbrazil@gmail.com>
2021-04-05 09:23:20 -0700

    formatting

--- mangen.py
+++ mangen.py
@@ -7,12 +7,8 @@ from jinja2 import Environment, FileSystemLoader
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
 template = env.get_template('manpage_template')
-
-# get parser info from jc.cli.about_jc()
-# plug it into the man page jinja2 template
 output = template.render(today=date.today(),
                          jc=jc.cli.about_jc())
 
-# save to man/jc.1
 with open('man/jc.1', 'w') as f:
     f.write(output)
--- readmegen.py
+++ readmegen.py
@@ -6,11 +6,7 @@ from jinja2 import Environment, FileSystemLoader
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
 template = env.get_template('readme_template')
-
-# get parser info from jc.cli.about_jc()
-# plug it into the man page jinja2 template
 output = template.render(jc=jc.cli.about_jc())
 
-# save to man/jc.1
 with open('README.md', 'w') as f:
     f.write(output)
