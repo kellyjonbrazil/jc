5500648aa0886ea5089856e5b33d7c8ebd78d5b0
Kelly Brazil <kellyjonbrazil@gmail.com>
2021-04-22 08:33:45 -0700

    fix ufw docs for int conversions and service behavior

--- jc/parsers/ufw.py
+++ jc/parsers/ufw.py
@@ -30,17 +30,17 @@ Schema:
           "to_ip":                  string,
           "to_ip_prefix":           integer,
           "to_interface":           string,
-          "to_transport":           string,     # null if to_service is set
-          "to_start_port":          integer,    # null if to_service is set
-          "to_end_port":            integer,    # null if to_service is set
-          "to_service":             string,     # null if any above are set
+          "to_transport":           string,     # null if to_service is set and not a list of ports
+          "to_start_port":          integer,    # null if to_service is set and not a list of ports
+          "to_end_port":            integer,    # null if to_service is set and not a list of ports
+          "to_service":             string,     # null if any above are set (can also be a list of ports)
           "from_ip":                string,
           "from_ip_prefix":         integer,
           "from_interface":         string,
-          "from_transport":         string,     # null if from_service is set
-          "from_start_port":        integer,    # null if from_service is set
-          "from_end_port":          integer,    # null if from_service is set
-          "from_service":           string,     # null if any above are set
+          "from_transport":         string,     # null if to_service is set and not a list of ports
+          "from_start_port":        integer,    # null if to_service is set and not a list of ports
+          "from_end_port":          integer,    # null if to_service is set and not a list of ports
+          "from_service":           string,     # null if any above are set (can also be a list of ports)
           "comment":                string      # null if no comment
         }
       ]
@@ -67,10 +67,10 @@ Examples:
           "to_end_port": 22,
           "to_service": null,
           "to_ip": "0.0.0.0",
-          "to_ip_prefix": "0",
+          "to_ip_prefix": 0,
           "comment": null,
           "from_ip": "0.0.0.0",
-          "from_ip_prefix": "0",
+          "from_ip_prefix": 0,
           "from_interface": "any",
           "from_transport": "any",
           "from_start_port": 0,
@@ -88,10 +88,10 @@ Examples:
           "to_end_port": 22,
           "to_service": null,
           "to_ip": "::",
-          "to_ip_prefix": "0",
+          "to_ip_prefix": 0,
           "comment": null,
           "from_ip": "::",
-          "from_ip_prefix": "0",
+          "from_ip_prefix": 0,
           "from_interface": "any",
           "from_transport": "any",
           "from_start_port": 0,
@@ -109,10 +109,10 @@ Examples:
           "to_start_port": null,
           "to_end_port": null,
           "to_ip": "0.0.0.0",
-          "to_ip_prefix": "0",
+          "to_ip_prefix": 0,
           "comment": null,
           "from_ip": "0.0.0.0",
-          "from_ip_prefix": "0",
+          "from_ip_prefix": 0,
           "from_interface": "any",
           "from_transport": "any",
           "from_start_port": 0,
@@ -126,14 +126,14 @@ Examples:
           "network_protocol": "ipv6",
           "to_interface": "any",
           "to_ip": "2405:204:7449:49fc:f09a:6f4a:bc93:1955",
-          "to_ip_prefix": "128",
+          "to_ip_prefix": 128,
           "to_transport": "any",
           "to_start_port": 0,
           "to_end_port": 65535,
           "to_service": null,
           "comment": null,
           "from_ip": "::",
-          "from_ip_prefix": "0",
+          "from_ip_prefix": 0,
           "from_interface": "any",
           "from_transport": "any",
           "from_start_port": 0,
@@ -147,14 +147,14 @@ Examples:
           "network_protocol": "ipv4",
           "to_interface": "en0",
           "to_ip": "10.10.10.10",
-          "to_ip_prefix": "32",
+          "to_ip_prefix": 32,
           "to_transport": "any",
           "to_start_port": 0,
           "to_end_port": 65535,
           "to_service": null,
           "comment": null,
           "from_ip": "0.0.0.0",
-          "from_ip_prefix": "0",
+          "from_ip_prefix": 0,
           "from_interface": "any",
           "from_transport": "any",
           "from_start_port": 0,
