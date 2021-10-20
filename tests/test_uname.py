2057817ef8fec85b2a978b682e7fe8bc16def828
Kelly Brazil <kellyjonbrazil@gmail.com>
2021-10-20 15:59:27 -0700

    add uname tests

--- tests/test_uname.py
+++ tests/test_uname.py
@@ -25,6 +25,21 @@ class MyTests(unittest.TestCase):
         with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/osx-10.14.6/uname.out'), 'r', encoding='utf-8') as f:
             self.osx_10_14_6_uname = f.read()
 
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/freebsd12/uname-a.out'), 'r', encoding='utf-8') as f:
+            self.freebsd12_uname_a = f.read()
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/freebsd12/uname-a2.out'), 'r', encoding='utf-8') as f:
+            self.freebsd12_uname_a2 = f.read()
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/uname-a.out'), 'r', encoding='utf-8') as f:
+            self.generic_uname_a = f.read()
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/uname-a-different-proc.out'), 'r', encoding='utf-8') as f:
+            self.generic_uname_a_different_proc = f.read()
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/debian10/uname-a.out'), 'r', encoding='utf-8') as f:
+            self.debian_10_uname_a = f.read()
+
         # output
         with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/centos-7.7/uname-a.json'), 'r', encoding='utf-8') as f:
             self.centos_7_7_uname_a_json = json.loads(f.read())
@@ -37,6 +52,21 @@ class MyTests(unittest.TestCase):
 
         with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/osx-10.14.6/uname-a.json'), 'r', encoding='utf-8') as f:
             self.osx_10_14_6_uname_a_json = json.loads(f.read())
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/freebsd12/uname-a.json'), 'r', encoding='utf-8') as f:
+            self.freebsd12_uname_a_json = json.loads(f.read())
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/freebsd12/uname-a2.json'), 'r', encoding='utf-8') as f:
+            self.freebsd12_uname_a2_json = json.loads(f.read())
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/uname-a.json'), 'r', encoding='utf-8') as f:
+            self.generic_uname_a_json = json.loads(f.read())
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/uname-a-different-proc.json'), 'r', encoding='utf-8') as f:
+            self.generic_uname_a_different_proc_json = json.loads(f.read())
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/debian10/uname-a.json'), 'r', encoding='utf-8') as f:
+            self.debian_10_uname_a_json = json.loads(f.read())
 
     def test_uname_nodata(self):
         """
@@ -73,6 +103,36 @@ class MyTests(unittest.TestCase):
         Test 'uname -a' on OSX 10.14.6
         """
         self.assertEqual(jc.parsers.uname.parse(self.osx_10_14_6_uname_a, quiet=True), self.osx_10_14_6_uname_a_json)
+
+    def test_uname_freebsd12(self):
+        """
+        Test 'uname -a' on freebsd12
+        """
+        self.assertEqual(jc.parsers.uname.parse(self.freebsd12_uname_a, quiet=True), self.freebsd12_uname_a_json)
+
+    def test_uname2_freebsd12(self):
+        """
+        Test 'uname -a' on freebsd12 with longer version level string
+        """
+        self.assertEqual(jc.parsers.uname.parse(self.freebsd12_uname_a2, quiet=True), self.freebsd12_uname_a2_json)
+
+    def test_uname_generic(self):
+        """
+        Test 'uname -a' on debian with missing hardware platform and processor
+        """
+        self.assertEqual(jc.parsers.uname.parse(self.generic_uname_a, quiet=True), self.generic_uname_a_json)
+
+    def test_uname_different_proc_generic(self):
+        """
+        Test 'uname -a' on machine with different processor type
+        """
+        self.assertEqual(jc.parsers.uname.parse(self.generic_uname_a_different_proc, quiet=True), self.generic_uname_a_different_proc_json)
+
+    def test_uname_debian_10(self):
+        """
+        Test 'uname -a' on debian 10 with missing hardware platform and processor
+        """
+        self.assertEqual(jc.parsers.uname.parse(self.debian_10_uname_a, quiet=True), self.debian_10_uname_a_json)
 
 
 if __name__ == '__main__':
