086da16b1743c16a5ccdd102f889fb31f99e3caa
Kelly Brazil <kellyjonbrazil@gmail.com>
2021-11-15 13:05:53 -0800

    version bump

--- jc/parsers/netstat.py
+++ jc/parsers/netstat.py
@@ -354,7 +354,7 @@ import jc.utils
 
 class info():
     """Provides parser metadata (version, author, etc.)"""
-    version = '1.10'
+    version = '1.11'
     description = '`netstat` command parser'
     author = 'Kelly Brazil'
     author_email = 'kellyjonbrazil@gmail.com'
