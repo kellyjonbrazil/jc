4f8c1a2ca2cb4c81938973f54c08bb4fa5f146c1
Kelly Brazil <kellyjonbrazil@gmail.com>
2022-04-27 09:43:55 -0700

    version bump

--- jc/parsers/update_alt_q.py
+++ jc/parsers/update_alt_q.py
@@ -132,7 +132,7 @@ import jc.utils
 
 class info():
     """Provides parser metadata (version, author, etc.)"""
-    version = '1.0'
+    version = '1.1'
     description = '`update-alternatives --query` command parser'
     author = 'Kelly Brazil'
     author_email = 'kellyjonbrazil@gmail.com'
