1763b530da2ae863e73f3642639c274c6257f069
Kelly Brazil <kellyjonbrazil@gmail.com>
2022-05-27 17:40:54 -0700

    add tests for new fields

--- tests/test_top.py
+++ tests/test_top.py
@@ -22,6 +22,9 @@ class MyTests(unittest.TestCase):
         with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/ubuntu-20.10/top-b-n1.out'), 'r', encoding='utf-8') as f:
             self.ubuntu_20_10_top_b_n1 = f.read()
 
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/ubuntu-20.10/top-b-allfields.out'), 'r', encoding='utf-8') as f:
+            self.ubuntu_20_10_top_b_allfields = f.read()
+
         # output
         with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/centos-7.7/top-b-n3.json'), 'r', encoding='utf-8') as f:
             self.centos_7_7_top_b_n3_json = json.loads(f.read())
@@ -34,6 +37,9 @@ class MyTests(unittest.TestCase):
 
         with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/ubuntu-20.10/top-b-n1.json'), 'r', encoding='utf-8') as f:
             self.ubuntu_20_10_top_b_n1_json = json.loads(f.read())
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/ubuntu-20.10/top-b-allfields.json'), 'r', encoding='utf-8') as f:
+            self.ubuntu_20_10_top_b_allfields_json = json.loads(f.read())
 
 
     def test_top_nodata(self):
@@ -65,6 +71,12 @@ class MyTests(unittest.TestCase):
         Test 'top -b -n 1' with units as MiB on Ubuntu 20.10
         """
         self.assertEqual(jc.parsers.top.parse(self.ubuntu_20_10_top_b_n1, quiet=True), self.ubuntu_20_10_top_b_n1_json)
+
+    def test_top_allfields_ubuntu_20_10(self):
+        """
+        Test 'top -b -n 1' with units as MiB and all fields on Ubuntu 20.10
+        """
+        self.assertEqual(jc.parsers.top.parse(self.ubuntu_20_10_top_b_allfields, quiet=True), self.ubuntu_20_10_top_b_allfields_json)
 
 
 if __name__ == '__main__':
--- tests/test_top_s.py
+++ tests/test_top_s.py
@@ -2,6 +2,7 @@ import os
 import json
 import unittest
 import jc.parsers.top_s
+from jc.exceptions import ParseError
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -25,6 +26,9 @@ class MyTests(unittest.TestCase):
         with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/ubuntu-20.10/top-b-n1.out'), 'r', encoding='utf-8') as f:
             self.ubuntu_20_10_top_b_n1 = f.read()
 
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/ubuntu-20.10/top-b-allfields.out'), 'r', encoding='utf-8') as f:
+            self.ubuntu_20_10_top_b_allfields = f.read()
+
         # output
         with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/centos-7.7/top-b-n3-streaming.json'), 'r', encoding='utf-8') as f:
             self.centos_7_7_top_b_n3_streaming_json = json.loads(f.read())
@@ -38,11 +42,20 @@ class MyTests(unittest.TestCase):
         with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/ubuntu-20.10/top-b-n1-streaming.json'), 'r', encoding='utf-8') as f:
             self.ubuntu_20_10_top_b_n1_streaming_json = json.loads(f.read())
 
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/ubuntu-20.10/top-b-allfields-streaming.json'), 'r', encoding='utf-8') as f:
+            self.ubuntu_20_10_top_b_allfields_streaming_json = json.loads(f.read())
+
     def test_top_s_nodata(self):
         """
         Test 'top' with no data
         """
         self.assertEqual(list(jc.parsers.top_s.parse([], quiet=True)), [])
+
+    def test_top_s_unparsable(self):
+        data = 'unparsable data'
+        g = jc.parsers.top_s.parse(data.splitlines(), quiet=True)
+        with self.assertRaises(ParseError):
+            list(g)
 
     def test_top_s_b_n3_centos_7_7(self):
         """
@@ -67,6 +80,12 @@ class MyTests(unittest.TestCase):
         Test 'top -b -n1' with MiB units on Ubuntu 20.10
         """
         self.assertEqual(list(jc.parsers.top_s.parse(self.ubuntu_20_10_top_b_n1.splitlines(), quiet=True)), self.ubuntu_20_10_top_b_n1_streaming_json)
+
+    def test_top_s_b_allfields_ubuntu_20_10(self):
+        """
+        Test 'top -b -n1' with MiB units and all fields on Ubuntu 20.10
+        """
+        self.assertEqual(list(jc.parsers.top_s.parse(self.ubuntu_20_10_top_b_allfields.splitlines(), quiet=True)), self.ubuntu_20_10_top_b_allfields_streaming_json)
 
 
 if __name__ == '__main__':
