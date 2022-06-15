247c43278cc95e5070e4a9e81257363a04fce5d9
Kelly Brazil <kellyjonbrazil@gmail.com>
2022-06-15 09:22:09 -0700

    add postconf tests

---
+++ tests/test_postconf.py
@@ -0,0 +1,35 @@
+import os
+import unittest
+import json
+import jc.parsers.postconf
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+class MyTests(unittest.TestCase):
+
+    def setUp(self):
+        # input
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/postconf-M.out'), 'r', encoding='utf-8') as f:
+            self.generic_postconf_m = f.read()
+
+        # output
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/postconf-M.json'), 'r', encoding='utf-8') as f:
+            self.generic_postconf_m_json = json.loads(f.read())
+
+
+    def test_postconf_nodata(self):
+        """
+        Test 'postconf' with no data
+        """
+        self.assertEqual(jc.parsers.postconf.parse('', quiet=True), [])
+
+    def test_postconf(self):
+        """
+        Test 'postconf -M'
+        """
+        self.assertEqual(jc.parsers.postconf.parse(self.generic_postconf_m, quiet=True), self.generic_postconf_m_json)
+
+
+if __name__ == '__main__':
+    unittest.main()
