26d4bbe9a460fc202910f61b4fb729b55f5fe0c6
Kelly Brazil <kellyjonbrazil@gmail.com>
2022-07-16 19:43:19 -0700

    add m3u tests

--- tests/test_m3u.py
+++ tests/test_m3u.py
@@ -13,9 +13,15 @@ class MyTests(unittest.TestCase):
         with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/m3u-example.m3u'), 'r', encoding='utf-8') as f:
             self.m3u_example = f.read()
 
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/m3u-dirty.m3u'), 'r', encoding='utf-8') as f:
+            self.m3u_dirty = f.read()
+
         # output
         with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/m3u-example.json'), 'r', encoding='utf-8') as f:
             self.m3u_example_json = json.loads(f.read())
+
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/m3u-dirty.json'), 'r', encoding='utf-8') as f:
+            self.m3u_dirty_json = json.loads(f.read())
 
 
     def test_m3u_nodata(self):
@@ -29,6 +35,12 @@ class MyTests(unittest.TestCase):
         Test 'm3u' example file
         """
         self.assertEqual(jc.parsers.m3u.parse(self.m3u_example, quiet=True), self.m3u_example_json)
+
+    def test_m3u_dirty(self):
+        """
+        Test 'm3u' example file with lots of difficult parsing
+        """
+        self.assertEqual(jc.parsers.m3u.parse(self.m3u_dirty, quiet=True), self.m3u_dirty_json)
 
 
 if __name__ == '__main__':
