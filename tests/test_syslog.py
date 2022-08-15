7c4cf66243c4896cf406ad9f94743a09c042af08
Kelly Brazil <kellyjonbrazil@gmail.com>
2022-08-15 16:54:10 -0700

    add syslog tests

---
+++ tests/test_syslog.py
@@ -0,0 +1,35 @@
+import os
+import unittest
+import json
+import jc.parsers.syslog
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+class MyTests(unittest.TestCase):
+
+    def setUp(self):
+        # input
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/syslog-5424.out'), 'r', encoding='utf-8') as f:
+            self.syslog = f.read()
+
+        # output
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/syslog-5424.json'), 'r', encoding='utf-8') as f:
+            self.syslog_json = json.loads(f.read())
+
+
+    def test_syslog_nodata(self):
+        """
+        Test 'syslog' with no data
+        """
+        self.assertEqual(jc.parsers.syslog.parse('', quiet=True), [])
+
+    def test_syslog_sample(self):
+        """
+        Test 'syslog' with sample data
+        """
+        self.assertEqual(jc.parsers.syslog.parse(self.syslog, quiet=True), self.syslog_json)
+
+
+if __name__ == '__main__':
+    unittest.main()
