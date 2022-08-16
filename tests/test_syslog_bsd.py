c82c6a88f8c3c388796d36d22f6eb3ce1a32c281
Kelly Brazil <kellyjonbrazil@gmail.com>
2022-08-15 18:10:48 -0700

    add tests

---
+++ tests/test_syslog_bsd.py
@@ -0,0 +1,35 @@
+import os
+import unittest
+import json
+import jc.parsers.syslog_bsd
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+class MyTests(unittest.TestCase):
+
+    def setUp(self):
+        # input
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/syslog-3164.out'), 'r', encoding='utf-8') as f:
+            self.syslog = f.read()
+
+        # output
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/syslog-3164.json'), 'r', encoding='utf-8') as f:
+            self.syslog_json = json.loads(f.read())
+
+
+    def test_syslog_bsd_nodata(self):
+        """
+        Test 'syslog_bsd' with no data
+        """
+        self.assertEqual(jc.parsers.syslog_bsd.parse('', quiet=True), [])
+
+    def test_syslog_bsd_sample(self):
+        """
+        Test 'syslog_bsd' with sample data
+        """
+        self.assertEqual(jc.parsers.syslog_bsd.parse(self.syslog, quiet=True), self.syslog_json)
+
+
+if __name__ == '__main__':
+    unittest.main()
