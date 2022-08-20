935e114d404444a30c5869150864babe642757a5
Kelly Brazil <kellyjonbrazil@gmail.com>
2022-08-20 11:25:56 -0700

    add syslog streaming tests

---
+++ tests/test_syslog_bsd_s.py
@@ -0,0 +1,37 @@
+import os
+import json
+import unittest
+import jc.parsers.syslog_bsd_s
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# To create streaming output use:
+# $ cat syslog.out | jc --syslog-bsd-s | jello -c > syslog-bsd-streaming.json
+
+
+class MyTests(unittest.TestCase):
+
+    def setUp(self):
+        # input
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/syslog-3164.out'), 'r', encoding='utf-8') as f:
+            self.syslog_bsd = f.read()
+
+        # output
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/syslog-3164-streaming.json'), 'r', encoding='utf-8') as f:
+            self.syslog_bsd_streaming_json = json.loads(f.read())
+
+    def test_syslog_bsd_s_nodata(self):
+        """
+        Test 'syslog_bsd' with no data
+        """
+        self.assertEqual(list(jc.parsers.syslog_bsd_s.parse([], quiet=True)), [])
+
+    def test_syslog_bsd_s(self):
+        """
+        Test bsd Syslog
+        """
+        self.assertEqual(list(jc.parsers.syslog_bsd_s.parse(self.syslog_bsd.splitlines(), quiet=True)), self.syslog_bsd_streaming_json)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_syslog_s.py
@@ -0,0 +1,37 @@
+import os
+import json
+import unittest
+import jc.parsers.syslog_s
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# To create streaming output use:
+# $ cat syslog.out | jc --syslog-s | jello -c > syslog-streaming.json
+
+
+class MyTests(unittest.TestCase):
+
+    def setUp(self):
+        # input
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/syslog-5424.out'), 'r', encoding='utf-8') as f:
+            self.syslog = f.read()
+
+        # output
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/syslog-5424-streaming.json'), 'r', encoding='utf-8') as f:
+            self.syslog_streaming_json = json.loads(f.read())
+
+    def test_syslog_s_nodata(self):
+        """
+        Test 'syslog' with no data
+        """
+        self.assertEqual(list(jc.parsers.syslog_s.parse([], quiet=True)), [])
+
+    def test_syslog_s(self):
+        """
+        Test syslog file
+        """
+        self.assertEqual(list(jc.parsers.syslog_s.parse(self.syslog.splitlines(), quiet=True)), self.syslog_streaming_json)
+
+
+if __name__ == '__main__':
+    unittest.main()
