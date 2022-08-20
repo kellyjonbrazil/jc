f7c6a82e7396f0cd9281a81d89fa9971d59c22a4
Kelly Brazil <kellyjonbrazil@gmail.com>
2022-08-20 13:38:20 -0700

    add cef tests

---
+++ tests/test_cef.py
@@ -0,0 +1,35 @@
+import os
+import unittest
+import json
+import jc.parsers.cef
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+class MyTests(unittest.TestCase):
+
+    def setUp(self):
+        # input
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/cef.out'), 'r', encoding='utf-8') as f:
+            self.cef = f.read()
+
+        # output
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/cef.json'), 'r', encoding='utf-8') as f:
+            self.cef_json = json.loads(f.read())
+
+
+    def test_cef_nodata(self):
+        """
+        Test 'cef' with no data
+        """
+        self.assertEqual(jc.parsers.cef.parse('', quiet=True), [])
+
+    def test_cef_sample(self):
+        """
+        Test with sample cef log
+        """
+        self.assertEqual(jc.parsers.cef.parse(self.cef, quiet=True), self.cef_json)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_cef_s.py
@@ -0,0 +1,37 @@
+import os
+import json
+import unittest
+import jc.parsers.cef_s
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# To create streaming output use:
+# $ cat cef.out | jc --cef-s | jello -c > cef-streaming.json
+
+
+class MyTests(unittest.TestCase):
+
+    def setUp(self):
+        # input
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/cef.out'), 'r', encoding='utf-8') as f:
+            self.cef = f.read()
+
+        # output
+        with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/cef-streaming.json'), 'r', encoding='utf-8') as f:
+            self.cef_streaming_json = json.loads(f.read())
+
+    def test_cef_s_nodata(self):
+        """
+        Test 'cef' with no data
+        """
+        self.assertEqual(list(jc.parsers.cef_s.parse([], quiet=True)), [])
+
+    def test_cef_s_sample(self):
+        """
+        Test with sample cef log
+        """
+        self.assertEqual(list(jc.parsers.cef_s.parse(self.cef.splitlines(), quiet=True)), self.cef_streaming_json)
+
+
+if __name__ == '__main__':
+    unittest.main()
