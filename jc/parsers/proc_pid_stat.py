0dd4b5f620ae55dd5bff16b4605c4293f7214414
Kelly Brazil <kellyjonbrazil@gmail.com>
2022-09-28 14:56:37 -0700

    parser version bump

--- jc/parsers/proc_pid_stat.py
+++ jc/parsers/proc_pid_stat.py
@@ -202,7 +202,7 @@ import jc.utils
 
 class info():
     """Provides parser metadata (version, author, etc.)"""
-    version = '1.0'
+    version = '1.1'
     description = '`/proc/<pid>/stat` file parser'
     author = 'Kelly Brazil'
     author_email = 'kellyjonbrazil@gmail.com'
