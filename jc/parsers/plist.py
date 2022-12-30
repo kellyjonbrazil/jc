e49c621e59e354dbe17160ea36ee190b44400fcb
Kelly Brazil <kellyjonbrazil@gmail.com>
2022-12-30 10:44:46 -0800

    try writing to file as bytes for windows compatibility

--- jc/parsers/plist.py
+++ jc/parsers/plist.py
@@ -166,8 +166,7 @@ def parse(
             # Try parsing as an old-style NeXTSTEP Plist format
             # pbPlist library only works on file paths, not strings :(
             import tempfile
-            with tempfile.NamedTemporaryFile(mode='w+') as plist_file:
-                data = data.decode()
+            with tempfile.NamedTemporaryFile(mode='w+b') as plist_file:
                 plist_file.write(data)
                 plist_file.seek(0)
                 parsed_plist = PBPlist(plist_file.name)
