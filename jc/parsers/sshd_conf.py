f8fbb2dce274ee92464e42cb134e5a8f5ee41e96
Kelly Brazil <kellyjonbrazil@gmail.com>
2023-01-22 10:36:29 -0800

    use derivative of sshd_conf parser instead of paramiko

--- jc/parsers/sshd_conf.py
+++ jc/parsers/sshd_conf.py
@@ -483,13 +483,13 @@ import jc.utils
 
 class info():
     """Provides parser metadata (version, author, etc.)"""
-    version = '1.0'
+    version = '1.1'
     description = 'sshd config file and `sshd -T` command parser'
     author = 'Kelly Brazil'
     author_email = 'kellyjonbrazil@gmail.com'
     compatible = ['linux', 'darwin', 'freebsd']
     magic_commands = ['sshd -T']
-    tags = ['file']
+    tags = ['command', 'file']
 
 
 __version__ = info.version
@@ -622,6 +622,10 @@ def parse(
     if jc.utils.has_data(data):
 
         for line in filter(None, data.splitlines()):
+            # skip any lines with only whitespace
+            if not line.strip():
+                continue
+
             # support configuration file by skipping commented lines
             if line.strip().startswith('#'):
                 continue
