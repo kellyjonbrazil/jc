1b3985c2d763c0c7b6d74c3c28606009cc83793f
Samson Umezulike <samson.ume@gmail.com>
2023-10-01 00:12:18 +0200

    Adds graceful handling of superfluous bits in bit strings (#459)
    Co-authored-by: Kelly Brazil <kellyjonbrazil@gmail.com>

--- tests/test_x509_cert.py
+++ tests/test_x509_cert.py
@@ -27,6 +27,9 @@ class MyTests(unittest.TestCase):
     with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/x509-cert-bad-email.pem'), 'rb') as f:
         x509_cert_bad_email = f.read()
 
+    with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/x509-cert-superfluous-bits.pem'), 'rb') as f:
+        x509_cert_superfluous_bits = f.read()
+
     with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/x509-negative-serial.pem'), 'rb') as f:
         x509_cert_negative_serial = f.read()
 
@@ -49,6 +52,9 @@ class MyTests(unittest.TestCase):
     with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/x509-cert-bad-email.json'), 'r', encoding='utf-8') as f:
         x509_cert_bad_email_json = json.loads(f.read())
 
+    with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/x509-cert-superfluous-bits.json'), 'r', encoding='utf-8') as f:
+        x509_cert_superfluous_bits_json = json.loads(f.read())
+        
     with open(os.path.join(THIS_DIR, os.pardir, 'tests/fixtures/generic/x509-negative-serial.json'), 'r', encoding='utf-8') as f:
         x509_cert_negative_serial_json = json.loads(f.read())
 
@@ -95,6 +101,12 @@ class MyTests(unittest.TestCase):
         """
         self.assertEqual(jc.parsers.x509_cert.parse(self.x509_cert_bad_email, quiet=True), self.x509_cert_bad_email_json)
 
+    def test_x509_cert_superfluous_bits(self):
+        """
+        Test 'cat x509-cert-superfluous-bits.pem' (PEM file with more bits set for the keyUsage extension than defined by the RFC)
+        """
+        self.assertEqual(jc.parsers.x509_cert.parse(self.x509_cert_superfluous_bits, quiet=True),
+                         self.x509_cert_superfluous_bits_json)
     def test_x509_cert_negative_serial(self):
         """
         Test 'cat x509-cert-bad-email.pem' (PEM file with a non-compliant email address)
