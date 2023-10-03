be175b71cae4dab3315f67e8101b4aac7655f114
pettai <pettai@sunet.se>
2023-10-03 21:44:07 +0200

    add more tests
    add all test-cases

--- tests/test_nsd_control.py
+++ tests/test_nsd_control.py
@@ -22,7 +22,16 @@ class MyTests(unittest.TestCase):
                 'fixtures/generic/nsd_control-status.json'),
             'zonestatus': (
                 'fixtures/generic/nsd_control-zonestatus.out',
-                'fixtures/generic/nsd_control-zonestatus.json')
+                'fixtures/generic/nsd_control-zonestatus.json'),
+            'print_cookie_secrets': (
+                'fixtures/generic/nsd_control-cookie_secrets.out',
+                'fixtures/generic/nsd_control-cookie_secrets.json'),
+            'print_tsig': (
+                'fixtures/generic/nsd_control-tsig.out',
+                'fixtures/generic/nsd_control-tsig.json'),
+            'stats': (
+                'fixtures/generic/nsd_control-stats.out',
+                'fixtures/generic/nsd_control-stats.json')
         }
 
         for file, filepaths in fixtures.items():
@@ -58,6 +67,33 @@ class MyTests(unittest.TestCase):
         self.assertEqual(
             parse(self.f_in['zonestatus'], quiet=True),
             self.f_json['zonestatus']
+        )
+
+    def test_nsd_control_cookie_secrets(self):
+        """
+        Test 'nsd-control print_cookie_secrets'
+        """
+        self.assertEqual(
+            parse(self.f_in['print_cookie_secrets'], quiet=True),
+            self.f_json['print_cookie_secrets']
+        )
+
+    def test_nsd_control_tsig(self):
+        """
+        Test 'nsd-control print_tsig'
+        """
+        self.assertEqual(
+            parse(self.f_in['print_tsig'], quiet=True),
+            self.f_json['print_tsig']
+        )
+
+    def test_nsd_control_stats(self):
+        """
+        Test 'nsd-control stats'
+        """
+        self.assertEqual(
+            parse(self.f_in['stats'], quiet=True),
+            self.f_json['stats']
         )
 
 if __name__ == '__main__':
