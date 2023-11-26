0b88f45a6b56795a96daa8fcf9a19b69439ebf2c
Roey Darwish Dror <roey.ghost@gmail.com>
2023-11-26 19:30:25 +0000

    swapon parser

--- jc/lib.py
+++ jc/lib.py
@@ -9,243 +9,248 @@ from .jc_types import ParserInfoType, JSONDictType
 from jc import appdirs
 
 
-__version__ = '1.23.6'
+__version__ = "1.23.6"
 
 parsers: List[str] = [
-    'acpi',
-    'airport',
-    'airport-s',
-    'arp',
-    'asciitable',
-    'asciitable-m',
-    'blkid',
-    'bluetoothctl',
-    'cbt',
-    'cef',
-    'cef-s',
-    'certbot',
-    'chage',
-    'cksum',
-    'clf',
-    'clf-s',
-    'crontab',
-    'crontab-u',
-    'csv',
-    'csv-s',
-    'date',
-    'datetime-iso',
-    'df',
-    'dig',
-    'dir',
-    'dmidecode',
-    'dpkg-l',
-    'du',
-    'email-address',
-    'env',
-    'file',
-    'find',
-    'findmnt',
-    'finger',
-    'free',
-    'fstab',
-    'git-log',
-    'git-log-s',
-    'git-ls-remote',
-    'gpg',
-    'group',
-    'gshadow',
-    'hash',
-    'hashsum',
-    'hciconfig',
-    'history',
-    'host',
-    'hosts',
-    'id',
-    'ifconfig',
-    'ini',
-    'ini-dup',
-    'iostat',
-    'iostat-s',
-    'ip-address',
-    'iptables',
-    'ip-route',
-    'iso-datetime',
-    'iw-scan',
-    'iwconfig',
-    'jar-manifest',
-    'jobs',
-    'jwt',
-    'kv',
-    'last',
-    'ls',
-    'ls-s',
-    'lsattr',
-    'lsb-release',
-    'lsblk',
-    'lsmod',
-    'lsof',
-    'lspci',
-    'lsusb',
-    'm3u',
-    'mdadm',
-    'mount',
-    'mpstat',
-    'mpstat-s',
-    'netstat',
-    'nmcli',
-    'nsd-control',
-    'ntpq',
-    'openvpn',
-    'os-prober',
-    'os-release',
-    'passwd',
-    'pci-ids',
-    'pgpass',
-    'pidstat',
-    'pidstat-s',
-    'ping',
-    'ping-s',
-    'pip-list',
-    'pip-show',
-    'plist',
-    'postconf',
-    'proc',
-    'proc-buddyinfo',
-    'proc-consoles',
-    'proc-cpuinfo',
-    'proc-crypto',
-    'proc-devices',
-    'proc-diskstats',
-    'proc-filesystems',
-    'proc-interrupts',
-    'proc-iomem',
-    'proc-ioports',
-    'proc-loadavg',
-    'proc-locks',
-    'proc-meminfo',
-    'proc-modules',
-    'proc-mtrr',
-    'proc-pagetypeinfo',
-    'proc-partitions',
-    'proc-slabinfo',
-    'proc-softirqs',
-    'proc-stat',
-    'proc-swaps',
-    'proc-uptime',
-    'proc-version',
-    'proc-vmallocinfo',
-    'proc-vmstat',
-    'proc-zoneinfo',
-    'proc-driver-rtc',
-    'proc-net-arp',
-    'proc-net-dev',
-    'proc-net-dev-mcast',
-    'proc-net-if-inet6',
-    'proc-net-igmp',
-    'proc-net-igmp6',
-    'proc-net-ipv6-route',
-    'proc-net-netlink',
-    'proc-net-netstat',
-    'proc-net-packet',
-    'proc-net-protocols',
-    'proc-net-route',
-    'proc-net-tcp',
-    'proc-net-unix',
-    'proc-pid-fdinfo',
-    'proc-pid-io',
-    'proc-pid-maps',
-    'proc-pid-mountinfo',
-    'proc-pid-numa-maps',
-    'proc-pid-smaps',
-    'proc-pid-stat',
-    'proc-pid-statm',
-    'proc-pid-status',
-    'ps',
-    'resolve-conf',
-    'route',
-    'rpm-qi',
-    'rsync',
-    'rsync-s',
-    'semver',
-    'sfdisk',
-    'shadow',
-    'srt',
-    'ss',
-    'ssh-conf',
-    'sshd-conf',
-    'stat',
-    'stat-s',
-    'sysctl',
-    'syslog',
-    'syslog-s',
-    'syslog-bsd',
-    'syslog-bsd-s',
-    'systemctl',
-    'systemctl-lj',
-    'systemctl-ls',
-    'systemctl-luf',
-    'systeminfo',
-    'time',
-    'timedatectl',
-    'timestamp',
-    'toml',
-    'top',
-    'top-s',
-    'tracepath',
-    'traceroute',
-    'udevadm',
-    'ufw',
-    'ufw-appinfo',
-    'uname',
-    'update-alt-gs',
-    'update-alt-q',
-    'upower',
-    'uptime',
-    'url',
-    'ver',
-    'veracrypt',
-    'vmstat',
-    'vmstat-s',
-    'w',
-    'wc',
-    'who',
-    'x509-cert',
-    'x509-csr',
-    'xml',
-    'xrandr',
-    'yaml',
-    'zipinfo',
-    'zpool-iostat',
-    'zpool-status'
+    "acpi",
+    "airport",
+    "airport-s",
+    "arp",
+    "asciitable",
+    "asciitable-m",
+    "blkid",
+    "bluetoothctl",
+    "cbt",
+    "cef",
+    "cef-s",
+    "certbot",
+    "chage",
+    "cksum",
+    "clf",
+    "clf-s",
+    "crontab",
+    "crontab-u",
+    "csv",
+    "csv-s",
+    "date",
+    "datetime-iso",
+    "df",
+    "dig",
+    "dir",
+    "dmidecode",
+    "dpkg-l",
+    "du",
+    "email-address",
+    "env",
+    "file",
+    "find",
+    "findmnt",
+    "finger",
+    "free",
+    "fstab",
+    "git-log",
+    "git-log-s",
+    "git-ls-remote",
+    "gpg",
+    "group",
+    "gshadow",
+    "hash",
+    "hashsum",
+    "hciconfig",
+    "history",
+    "host",
+    "hosts",
+    "id",
+    "ifconfig",
+    "ini",
+    "ini-dup",
+    "iostat",
+    "iostat-s",
+    "ip-address",
+    "iptables",
+    "ip-route",
+    "iso-datetime",
+    "iw-scan",
+    "iwconfig",
+    "jar-manifest",
+    "jobs",
+    "jwt",
+    "kv",
+    "last",
+    "ls",
+    "ls-s",
+    "lsattr",
+    "lsb-release",
+    "lsblk",
+    "lsmod",
+    "lsof",
+    "lspci",
+    "lsusb",
+    "m3u",
+    "mdadm",
+    "mount",
+    "mpstat",
+    "mpstat-s",
+    "netstat",
+    "nmcli",
+    "nsd-control",
+    "ntpq",
+    "openvpn",
+    "os-prober",
+    "os-release",
+    "passwd",
+    "pci-ids",
+    "pgpass",
+    "pidstat",
+    "pidstat-s",
+    "ping",
+    "ping-s",
+    "pip-list",
+    "pip-show",
+    "plist",
+    "postconf",
+    "proc",
+    "proc-buddyinfo",
+    "proc-consoles",
+    "proc-cpuinfo",
+    "proc-crypto",
+    "proc-devices",
+    "proc-diskstats",
+    "proc-filesystems",
+    "proc-interrupts",
+    "proc-iomem",
+    "proc-ioports",
+    "proc-loadavg",
+    "proc-locks",
+    "proc-meminfo",
+    "proc-modules",
+    "proc-mtrr",
+    "proc-pagetypeinfo",
+    "proc-partitions",
+    "proc-slabinfo",
+    "proc-softirqs",
+    "proc-stat",
+    "proc-swaps",
+    "proc-uptime",
+    "proc-version",
+    "proc-vmallocinfo",
+    "proc-vmstat",
+    "proc-zoneinfo",
+    "proc-driver-rtc",
+    "proc-net-arp",
+    "proc-net-dev",
+    "proc-net-dev-mcast",
+    "proc-net-if-inet6",
+    "proc-net-igmp",
+    "proc-net-igmp6",
+    "proc-net-ipv6-route",
+    "proc-net-netlink",
+    "proc-net-netstat",
+    "proc-net-packet",
+    "proc-net-protocols",
+    "proc-net-route",
+    "proc-net-tcp",
+    "proc-net-unix",
+    "proc-pid-fdinfo",
+    "proc-pid-io",
+    "proc-pid-maps",
+    "proc-pid-mountinfo",
+    "proc-pid-numa-maps",
+    "proc-pid-smaps",
+    "proc-pid-stat",
+    "proc-pid-statm",
+    "proc-pid-status",
+    "ps",
+    "resolve-conf",
+    "route",
+    "rpm-qi",
+    "rsync",
+    "rsync-s",
+    "semver",
+    "sfdisk",
+    "shadow",
+    "srt",
+    "ss",
+    "ssh-conf",
+    "sshd-conf",
+    "stat",
+    "stat-s",
+    "swapon",
+    "sysctl",
+    "syslog",
+    "syslog-s",
+    "syslog-bsd",
+    "syslog-bsd-s",
+    "systemctl",
+    "systemctl-lj",
+    "systemctl-ls",
+    "systemctl-luf",
+    "systeminfo",
+    "time",
+    "timedatectl",
+    "timestamp",
+    "toml",
+    "top",
+    "top-s",
+    "tracepath",
+    "traceroute",
+    "udevadm",
+    "ufw",
+    "ufw-appinfo",
+    "uname",
+    "update-alt-gs",
+    "update-alt-q",
+    "upower",
+    "uptime",
+    "url",
+    "ver",
+    "veracrypt",
+    "vmstat",
+    "vmstat-s",
+    "w",
+    "wc",
+    "who",
+    "x509-cert",
+    "x509-csr",
+    "xml",
+    "xrandr",
+    "yaml",
+    "zipinfo",
+    "zpool-iostat",
+    "zpool-status",
 ]
+
 
 def _cliname_to_modname(parser_cli_name: str) -> str:
     """Return real module name (dashes converted to underscores)"""
-    return parser_cli_name.replace('--', '').replace('-', '_')
+    return parser_cli_name.replace("--", "").replace("-", "_")
+
 
 def _modname_to_cliname(parser_mod_name: str) -> str:
     """Return module's cli name (underscores converted to dashes)"""
-    return parser_mod_name.replace('_', '-')
+    return parser_mod_name.replace("_", "-")
+
 
 def _is_valid_parser_plugin(name: str, local_parsers_dir: str) -> bool:
-    if re.match(r'\w+\.py$', name) and os.path.isfile(os.path.join(local_parsers_dir, name)):
+    if re.match(r"\w+\.py$", name) and os.path.isfile(os.path.join(local_parsers_dir, name)):
         try:
             parser_mod_name = _cliname_to_modname(name)[0:-3]
-            modpath = 'jcparsers.'
-            plugin =  importlib.import_module(f'{modpath}{parser_mod_name}')
-            if hasattr(plugin, 'info') and hasattr(plugin, 'parse'):
+            modpath = "jcparsers."
+            plugin = importlib.import_module(f"{modpath}{parser_mod_name}")
+            if hasattr(plugin, "info") and hasattr(plugin, "parse"):
                 del plugin
                 return True
         except Exception:
             return False
     return False
 
+
 # Create the local_parsers list. This is a list of custom or
 # override parsers from <user_data_dir>/jc/jcparsers/*.py.
 # Once this list is created, extend the parsers list with it.
 local_parsers: List[str] = []
-data_dir = appdirs.user_data_dir('jc', 'jc')  # type: ignore
-local_parsers_dir = os.path.join(data_dir, 'jcparsers')
+data_dir = appdirs.user_data_dir("jc", "jc")  # type: ignore
+local_parsers_dir = os.path.join(data_dir, "jcparsers")
 if os.path.isdir(local_parsers_dir):
     sys.path.append(data_dir)
     for name in os.listdir(local_parsers_dir):
@@ -259,18 +264,21 @@ if os.path.isdir(local_parsers_dir):
     except Exception:
         pass
 
+
 def _parser_argument(parser_mod_name: str) -> str:
     """Return short name of the parser with dashes and with -- prefix"""
     parser = _modname_to_cliname(parser_mod_name)
-    return f'--{parser}'
+    return f"--{parser}"
+
 
 def _get_parser(parser_mod_name: str) -> ModuleType:
     """Return the parser module object"""
     # ensure parser_mod_name is a true module name and not a cli name
     parser_mod_name = _cliname_to_modname(parser_mod_name)
     parser_cli_name = _modname_to_cliname(parser_mod_name)
-    modpath: str = 'jcparsers.' if parser_cli_name in local_parsers else 'jc.parsers.'
-    return importlib.import_module(f'{modpath}{parser_mod_name}')
+    modpath: str = "jcparsers." if parser_cli_name in local_parsers else "jc.parsers."
+    return importlib.import_module(f"{modpath}{parser_mod_name}")
+
 
 def _parser_is_streaming(parser: ModuleType) -> bool:
     """
@@ -278,10 +286,11 @@ def _parser_is_streaming(parser: ModuleType) -> bool:
 
     parser is a parser module object.
     """
-    if getattr(parser.info, 'streaming', None):
+    if getattr(parser.info, "streaming", None):
         return True
 
     return False
+
 
 def _parser_is_hidden(parser: ModuleType) -> bool:
     """
@@ -289,10 +298,11 @@ def _parser_is_hidden(parser: ModuleType) -> bool:
 
     parser is a parser module object.
     """
-    if getattr(parser.info, 'hidden', None):
+    if getattr(parser.info, "hidden", None):
         return True
 
     return False
+
 
 def _parser_is_deprecated(parser: ModuleType) -> bool:
     """
@@ -300,10 +310,11 @@ def _parser_is_deprecated(parser: ModuleType) -> bool:
 
     parser is a parser module object.
     """
-    if getattr(parser.info, 'deprecated', None):
+    if getattr(parser.info, "deprecated", None):
         return True
 
     return False
+
 
 def parse(
     parser_mod_name: Union[str, ModuleType],
@@ -311,7 +322,7 @@ def parse(
     quiet: bool = False,
     raw: bool = False,
     ignore_exceptions: Optional[bool] = None,
-    **kwargs
+    **kwargs,
 ) -> Union[JSONDictType, List[JSONDictType], Iterator[JSONDictType]]:
     """
     Parse the data (string or bytes) using the supplied parser (string or
@@ -397,20 +408,12 @@ def parse(
         jc_parser = _get_parser(parser_mod_name)
 
     if ignore_exceptions is not None:
-        return jc_parser.parse(
-            data,
-            quiet=quiet,
-            raw=raw,
-            ignore_exceptions=ignore_exceptions,
-            **kwargs
-        )
+        return jc_parser.parse(data, quiet=quiet, raw=raw, ignore_exceptions=ignore_exceptions, **kwargs)
 
     return jc_parser.parse(data, quiet=quiet, raw=raw, **kwargs)
 
-def parser_mod_list(
-    show_hidden: bool = False,
-    show_deprecated: bool = False
-) -> List[str]:
+
+def parser_mod_list(show_hidden: bool = False, show_deprecated: bool = False) -> List[str]:
     """Returns a list of all available parser module names."""
     plist: List[str] = []
     for p in parsers:
@@ -426,10 +429,8 @@ def parser_mod_list(
 
     return plist
 
-def plugin_parser_mod_list(
-    show_hidden: bool = False,
-    show_deprecated: bool = False
-) -> List[str]:
+
+def plugin_parser_mod_list(show_hidden: bool = False, show_deprecated: bool = False) -> List[str]:
     """
     Returns a list of plugin parser module names. This function is a
     subset of `parser_mod_list()`.
@@ -448,10 +449,8 @@ def plugin_parser_mod_list(
 
     return plist
 
-def standard_parser_mod_list(
-    show_hidden: bool = False,
-    show_deprecated: bool = False
-) -> List[str]:
+
+def standard_parser_mod_list(show_hidden: bool = False, show_deprecated: bool = False) -> List[str]:
     """
     Returns a list of standard parser module names. This function is a
     subset of `parser_mod_list()` and does not contain any streaming
@@ -462,7 +461,6 @@ def standard_parser_mod_list(
         parser = _get_parser(p)
 
         if not _parser_is_streaming(parser):
-
             if not show_hidden and _parser_is_hidden(parser):
                 continue
 
@@ -473,10 +471,8 @@ def standard_parser_mod_list(
 
     return plist
 
-def streaming_parser_mod_list(
-    show_hidden: bool = False,
-    show_deprecated: bool = False
-) -> List[str]:
+
+def streaming_parser_mod_list(show_hidden: bool = False, show_deprecated: bool = False) -> List[str]:
     """
     Returns a list of streaming parser module names. This function is a
     subset of `parser_mod_list()`.
@@ -486,7 +482,6 @@ def streaming_parser_mod_list(
         parser = _get_parser(p)
 
         if _parser_is_streaming(parser):
-
             if not show_hidden and _parser_is_hidden(parser):
                 continue
 
@@ -497,10 +492,8 @@ def streaming_parser_mod_list(
 
     return plist
 
-def parser_info(
-    parser_mod_name: Union[str, ModuleType],
-    documentation: bool = False
-) -> ParserInfoType:
+
+def parser_info(parser_mod_name: Union[str, ModuleType], documentation: bool = False) -> ParserInfoType:
     """
     Returns a dictionary that includes the parser module metadata.
 
@@ -516,7 +509,7 @@ def parser_info(
     """
     if isinstance(parser_mod_name, ModuleType):
         parser_mod = parser_mod_name
-        parser_mod_name = parser_mod.__name__.split('.')[-1]
+        parser_mod_name = parser_mod.__name__.split(".")[-1]
     else:
         # ensure parser_mod_name is a true module name and not a cli name
         parser_mod_name = _cliname_to_modname(parser_mod_name)
@@ -524,30 +517,29 @@ def parser_info(
 
     info_dict: ParserInfoType = {}
 
-    if hasattr(parser_mod, 'info'):
-        info_dict['name'] = parser_mod_name
-        info_dict['argument'] = _parser_argument(parser_mod_name)
+    if hasattr(parser_mod, "info"):
+        info_dict["name"] = parser_mod_name
+        info_dict["argument"] = _parser_argument(parser_mod_name)
         parser_entry = vars(parser_mod.info)
 
         for k, v in parser_entry.items():
-            if not k.startswith('__'):
+            if not k.startswith("__"):
                 info_dict[k] = v  # type: ignore
 
         if _modname_to_cliname(parser_mod_name) in local_parsers:
-            info_dict['plugin'] = True
+            info_dict["plugin"] = True
 
         if documentation:
             docs = parser_mod.__doc__
             if not docs:
-                docs = 'No documentation available.\n'
-            info_dict['documentation'] = docs
+                docs = "No documentation available.\n"
+            info_dict["documentation"] = docs
 
     return info_dict
 
+
 def all_parser_info(
-    documentation: bool = False,
-    show_hidden: bool = False,
-    show_deprecated: bool = False
+    documentation: bool = False, show_hidden: bool = False, show_deprecated: bool = False
 ) -> List[ParserInfoType]:
     """
     Returns a list of dictionaries that includes metadata for all parser
@@ -577,6 +569,7 @@ def all_parser_info(
     p_info_list: List[ParserInfoType] = [parser_info(p, documentation=documentation) for p in plist]
 
     return p_info_list
+
 
 def get_help(parser_mod_name: Union[str, ModuleType]) -> None:
     """
