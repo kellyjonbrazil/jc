0ef9ed273a40de566b3f016a3a493e83ba8e35db
Ron Green <11993626+georgettica@users.noreply.github.com>
2023-11-30 17:39:50 +0200

    add more iftop fun

--- jc/parsers/iftop.py
+++ jc/parsers/iftop.py
@@ -195,7 +195,106 @@ def parse(
 
     # clients: List = []
 
-    re_linux_device = re.compile(r'''
-    Listening\son\s(?P<device>.+)
-    ''', re.IGNORECASE | re.VERBOSE)
+    """
+    interface: enp0s3
+    IP address is: 10.10.15.129
+    MAC address is: 08:00:27:c0:4a:4f
+    Listening on enp0s3
+    # Host name (port/service if enabled)            last 2s   last 10s   last 40s cumulative
+    --------------------------------------------------------------------------------------------
+    1 ubuntu-2004-clean-01:ssh                 =>       448b       448b       448b       112B
+        10.10.15.72:40876                        <=       208b       208b       208b        52B
+    --------------------------------------------------------------------------------------------
+    Total send rate:                                       448b       448b       448b
+    Total receive rate:                                    208b       208b       208b
+    Total send and receive rate:                           656b       656b       656b
+    --------------------------------------------------------------------------------------------
+    Peak rate (sent/received/total):                       448b       208b       656b
+    Cumulative (sent/received/total):                      112B        52B       164B
+    ============================================================================================                 
+    """
 
+    re_linux_device = re.compile(r'interface:\s+(?P<device>.+)')
+    re_linux_ip = re.compile(r'IP address is:\s+(?P<ip_address>.+)')
+    re_linux_mac = re.compile(r'MAC address is:\s+(?P<mac_address>.+)')
+    before_arrow = r'(?P<index>\d+)\s+(?P<host_name>.+):(?P<host_port>\w+)\s+'
+    after_arrow_before_newline = r'\s+(?P<send_last_2s>[^\s]+)\s+(?P<send_last_10s>[^\s]+)\s+(?P<send_last_40s>[^\s]+)\s+(?P<send_cumulative>[^\s]+)'
+    newline_before_arrow = r'\s+(?P<receive_ip>.+):(?P<receive_port>\w+)\s+'
+    after_arrow_till_end =r'\s+(?P<receive_last_2s>[^\s]+)\s+(?P<receive_last_10s>[^\s]+)\s+(?P<receive_last_40s>[^\s]+)\s+(?P<receive_cumulative>[^\s]+)'
+    re_linux_clients = re.compile(f'{before_arrow}=>{after_arrow_before_newline}{newline_before_arrow}<={after_arrow_till_end}', re.MULTILINE)
+    re_total_send_rate = re.compile(r'Total send rate:\s+(?P<total_send_rate_last_2s>[^\s]+)\s+(?P<total_send_rate_last_10s>[^\s]+)\s+(?P<total_send_rate_last_40s>[^\s]+)')
+    re_total_receive_rate = re.compile(r'Total receive rate:\s+(?P<total_receive_rate_last_2s>[^\s]+)\s+(?P<total_receive_rate_last_10s>[^\s]+)\s+(?P<total_receive_rate_last_40s>[^\s]+)')
+    re_total_send_and_receive_rate = re.compile(r'Total send and receive rate:\s+(?P<total_send_and_receive_rate_last_2s>[^\s]+)\s+(?P<total_send_and_receive_rate_last_10s>[^\s]+)\s+(?P<total_send_and_receive_rate_last_40s>[^\s]+)')
+    re_peak_rate = re.compile(r'Peak rate \(sent/received/total\):\s+(?P<peak_rate_sent>[^\s]+)\s+(?P<peak_rate_received>[^\s]+)\s+(?P<peak_rate_total>[^\s]+)')
+    re_cumulative_rate = re.compile(r'Cumulative \(sent/received/total\):\s+(?P<cumulative_rate_sent>[^\s]+)\s+(?P<cumulative_rate_received>[^\s]+)\s+(?P<cumulative_rate_total>[^\s]+)')
+
+    if not jc.utils.has_data(data):
+        return []
+
+    linux_device_match = re_linux_device.match(data)["device"]
+    linux_ip_match = re_linux_ip.match(data)["ip_address"]
+    linux_mac_match = re_linux_mac.match(data)["mac_address"]
+
+    total_send_rate_match = re_total_send_rate.match(data)
+    total_receive_rate_match = re_total_receive_rate.match(data)
+    total_send_and_receive_rate_match = re_total_send_and_receive_rate.match(data)
+
+    peak_rate_match = re_peak_rate.match(data)
+    cumulative_rate_match = re_cumulative_rate.match(data)
+
+    interface_obj["device"] = linux_device_match
+    interface_obj["ip_address"] = linux_ip_match
+    interface_obj["mac_address"] = linux_mac_match
+    
+    interface_obj["clients"] = []
+    print(data)
+    # breakpoint()
+    
+    matches = re_linux_clients.finditer(data)
+    for match in matches:
+        current_client = {}
+        groupdict = match.groupdict()
+        current_client["index"] = groupdict["index"]
+        current_client["send_host_name"] = groupdict["host_name"]
+        current_client["send_host_port"] = groupdict["host_port"]
+        current_client["send_last_2s"] = groupdict["send_last_2s"]
+        current_client["send_last_10s"] = groupdict["send_last_10s"]
+        current_client["send_last_40s"] = groupdict["send_last_40s"]
+        current_client["send_cumulative"] = groupdict["send_cumulative"]
+        current_client["receive_ip"] = groupdict["receive_ip"]
+        current_client["receive_port"] = groupdict["receive_port"]
+        current_client["receive_last_2s"] = groupdict["receive_last_2s"]
+        current_client["receive_last_10s"] = groupdict["receive_last_10s"]
+        current_client["receive_last_40s"] = groupdict["receive_last_40s"]
+        current_client["receive_cumulative"] = groupdict["receive_cumulative"]
+        interface_obj["clients"].append(current_client)
+
+    interface_obj["total_send_rate"] = {
+        "last_2s": total_send_rate_match["total_send_rate_last_2s"],
+        "last_10s": total_send_rate_match["total_send_rate_last_10s"],
+        "last_40s": total_send_rate_match["total_send_rate_last_40s"],
+    }
+
+    interface_obj["total_receive_rate"] = {
+        "last_2s": total_receive_rate_match["total_receive_rate_last_2s"],
+        "last_10s": total_receive_rate_match["total_receive_rate_last_10s"],
+        "last_40s": total_receive_rate_match["total_receive_rate_last_40s"],
+    }
+
+    interface_obj["total_send_and_receive_rate"] = {
+        "last_2s": total_send_and_receive_rate_match["total_send_and_receive_rate_last_2s"],
+        "last_10s": total_send_and_receive_rate_match["total_send_and_receive_rate_last_10s"],
+        "last_40s": total_send_and_receive_rate_match["total_send_and_receive_rate_last_40s"],
+    }
+
+    interface_obj["peak_rate"] = {
+        "last_2s": peak_rate_match["peak_rate_sent"],
+        "last_10s": peak_rate_match["peak_rate_received"],
+        "last_40s": peak_rate_match["peak_rate_total"],
+    }
+
+    interface_obj["cumulative_rate"] = {
+        "last_2s": cumulative_rate_match["cumulative_rate_sent"],
+        "last_10s": cumulative_rate_match["cumulative_rate_received"],
+        "last_40s": cumulative_rate_match["cumulative_rate_total"],
+    }
