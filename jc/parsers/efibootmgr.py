d9400ad3601bc80758d3146b250f71b22da37e79
Yaofei Zheng <yaofei.zheng@dell.com>
2024-02-02 00:58:12 -0500

    Multiple fixes on jc/parsers/efibootmgr.py
    1. Update schema in comments.
    1. Add an example.
    1. Use clean and clear sub string finding sytax.
    1. Wrap schema in an object.
    1. Make variable scope as small as possible.

--- jc/parsers/efibootmgr.py
+++ jc/parsers/efibootmgr.py
@@ -18,22 +18,72 @@ Usage (module):
 
 Schema:
 
-    [
-      "BootCurrent": "0000",
-      "Timeout": "0 seconds",
-      "BootOrder": ["0000","0001"],
-      "BootOptions": [
-        {
-          "BootOptionReference": "Boot0000",
-          "DisplayName": "opensuse-secureboot",
-          "UefiDevicePath": "HD(1,GPT,e45dbd72-ba93-4810-9330-2c1c493ff08b,0x800,0x31800)/File(\EFI\opensuse\shim.efi)",
-          "BootOptionEnabled": true
-        }
-      ]
-    ]
+[
+    {
+        "BootCurrent": string,
+        "Timeout": string,
+        "BootOrder": [
+            string,
+            string,
+            string
+        ],
+        "BootOptions": [
+            {
+                "BootOptionReference": string,
+                "DisplayName": string,
+                "UefiDevicePath": string,
+                "BootOptionEnabled": bool
+            },
+            {
+                "BootOptionReference": string,
+                "DisplayName": string,
+                "UefiDevicePath": string,
+                "BootOptionEnabled": bool
+            },
+            {
+                "BootOptionReference": string,
+                "DisplayName": string,
+                "UefiDevicePath": string,
+                "BootOptionEnabled": bool
+            }
+        ]
+    }
+]
 
 Examples:
 
+$ sudo efibootmgr -v | jc --efibootmgr | jq
+[
+  {
+    "BootCurrent": "0002",
+    "Timeout": "0 seconds",
+    "BootOrder": [
+      "0002",
+      "0000",
+      "0001"
+    ],
+    "BootOptions": [
+      {
+        "BootOptionReference": "Boot0000",
+        "DisplayName": "WARNADO",
+        "UefiDevicePath": "HD(1,GPT,05b9944c-1c60-492b-a510-7bbedccdc602,0x800,0xfa000)/File(EFI\\boot\\bootx64.efi)",
+        "BootOptionEnabled": true
+      },
+      {
+        "BootOptionReference": "Boot0001",
+        "DisplayName": "Embedded NIC 1 Port 1 Partition 1",
+        "UefiDevicePath": "VenHw(3a191845-5f86-4e78-8fce-c4cff59f9daa)",
+        "BootOptionEnabled": true
+      },
+      {
+        "BootOptionReference": "Boot0002",
+        "DisplayName": "opensuse-secureboot",
+        "UefiDevicePath": "HD(1,GPT,c5d4f69d-6fc2-48c7-acee-af3f30336dc5,0x800,0x19000)/File(\\EFI\\opensuse\\shim.efi)",
+        "BootOptionEnabled": true
+      }
+    ]
+  }
+]
 
 """
 
@@ -59,39 +109,34 @@ def parse(data, raw=False, quiet=False):
     jc.utils.compatibility(__name__, info.compatible, quiet)
     jc.utils.input_type_check(data)
 
-    raw_output=[]
-    boot_opt_list=[]
+    raw_output = []
 
     if jc.utils.has_data(data):
+        boot_opt_list = []
+        efibootmgr_dict = {}
         for line in filter(None, data.splitlines()):
-            if -1 != line.find("BootCurrent"):
-                tmp_dict={}
-                tmp_dict["BootCurrent"]=line.split(':')[1].strip()
-                raw_output.append(tmp_dict)
-            elif -1 != line.find("Timeout"):
-                tmp_dict={}
-                tmp_dict["Timeout"]=line.split(':')[1].strip()
-                raw_output.append(tmp_dict)
-            elif -1 != line.find("BootOrder"):
-                tmp_dict={}
-                tmp_dict["BootOrder"]=line.split(':')[1].strip().split(',')
-                raw_output.append(tmp_dict)
-            elif -1 != line.find("Boot"):
-                tmp_dict={}
-                boot_record=line.split("\t")
-                tmp_dict["BootOptionReference"]=boot_record[0][0:8]
-                tmp_dict["DisplayName"]=boot_record[0][10:-1]
+            if "BootCurrent" in line:
+                efibootmgr_dict["BootCurrent"] = line.split(':')[1].strip()
+            elif "Timeout" in line:
+                efibootmgr_dict["Timeout"] = line.split(':')[1].strip()
+            elif "BootOrder" in line:
+                efibootmgr_dict["BootOrder"] = line.split(':')[1].strip().split(',')
+            elif "Boot" in line:
+                tmp_dict = {}
+                boot_record = line.split("\t")
+                tmp_dict["BootOptionReference"] = boot_record[0][0:8]
+                tmp_dict["DisplayName"] = boot_record[0][10:]
                 if len(boot_record) > 1:
-                    tmp_dict["UefiDevicePath"]=boot_record[1]
-                tmp_dict["BootOptionEnabled"]=boot_record[0][8] == '*'
+                    tmp_dict["UefiDevicePath"] = boot_record[1]
+                tmp_dict["BootOptionEnabled"] = boot_record[0][8] == '*'
                 boot_opt_list.append(tmp_dict)
             else:
                 # print(line)
-                pass
+                continue
 
-    tmp_dict={}
-    tmp_dict["BootOptions"]=boot_opt_list
-    raw_output.append(tmp_dict)
+        efibootmgr_dict["BootOptions"] = boot_opt_list
+
+        raw_output.append(efibootmgr_dict)
 
     if raw:
         return raw_output
