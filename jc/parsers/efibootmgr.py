3898c81712dd0b3839996ad7d6796137a5c24f78
Kelly Brazil <kellyjonbrazil@gmail.com>
2024-02-05 11:35:59 -0800

    efibootmgr updates

--- jc/parsers/efibootmgr.py
+++ jc/parsers/efibootmgr.py
@@ -1,6 +1,6 @@
 """jc - JSON Convert `efibootmgr` command output parser
 
-Only `efibootmgr -v` option is supported with the default output.
+The `-v` option is also supported.
 
 Usage (cli):
 
@@ -18,143 +18,150 @@ Usage (module):
 
 Schema:
 
-[
     {
-        "boot_current": string,
-        "timeout_seconds": number,
+        "boot_current":                     string,
+        "timeout_seconds":                  integer,
         "boot_order": [
-            string,
-            string,
-            string
+                                            string
         ],
-        mirrored_percentage_above_4g: number,
-        mirror_memory_below_4gb: bool,
+        mirrored_percentage_above_4g:       float,
+        mirror_memory_below_4gb:            boolean,
         "boot_options": [
             {
-                "boot_option_reference": string,
-                "display_name": string,
-                "uefi_device_path": string,
-                "boot_option_enabled": bool
-            },
-            {
-                "boot_option_reference": string,
-                "display_name": string,
-                "uefi_device_path": string,
-                "boot_option_enabled": bool
-            },
-            {
-                "boot_option_reference": string,
-                "display_name": string,
-                "uefi_device_path": string,
-                "boot_option_enabled": bool
+                "boot_option_reference":    string,
+                "display_name":             string,
+                "uefi_device_path":         string,
+                "boot_option_enabled":      boolean
             }
         ]
     }
-]
 
 Examples:
 
-$ sudo efibootmgr -v | jc --efibootmgr --pretty
-[
-  {
-    "boot_current": "0002",
-    "timeout_seconds": 0,
-    "boot_order": [
-      "0002",
-      "0000",
-      "0001"
-    ],
-    "mirrored_percentage_above_4g": 0.0,
-    "mirror_memory_below_4gb": false,
-    "boot_options": [
-      {
-        "boot_option_reference": "Boot0000",
-        "display_name": "WARNADO",
-        "uefi_device_path": "HD(1,GPT,05b9944c-1c60-492b-a510-7bbedccdc602,0x800,0xfa000)/File(EFI\\boot\\bootx64.efi)",
-        "boot_option_enabled": true
-      },
-      {
-        "boot_option_reference": "Boot0001",
-        "display_name": "Embedded NIC 1 Port 1 Partition 1",
-        "uefi_device_path": "VenHw(3a191845-5f86-4e78-8fce-c4cff59f9daa)",
-        "boot_option_enabled": true
-      },
-      {
-        "boot_option_reference": "Boot0002",
-        "display_name": "opensuse-secureboot",
-        "uefi_device_path": "HD(1,GPT,c5d4f69d-6fc2-48c7-acee-af3f30336dc5,0x800,0x19000)/File(\\EFI\\opensuse\\shim.efi)",
-        "boot_option_enabled": true
-      }
-    ]
-  }
-]
-
+    $ sudo efibootmgr -v | jc --efibootmgr --p
+    {
+      "boot_current": "0002",
+      "timeout_seconds": 0,
+      "boot_order": [
+        "0002",
+        "0000",
+        "0001"
+      ],
+      "mirrored_percentage_above_4g": 0.0,
+      "mirror_memory_below_4gb": false,
+      "boot_options": [
+        {
+          "boot_option_reference": "Boot0000",
+          "display_name": "WARNADO",
+          "uefi_device_path": "HD(1,GPT,05b9944c-1c60-492b-a510-7bbedccdc...",
+          "boot_option_enabled": true
+        },
+        {
+          "boot_option_reference": "Boot0001",
+          "display_name": "Embedded NIC 1 Port 1 Partition 1",
+          "uefi_device_path": "VenHw(3a191845-5f86-4e78-8fce-c4cff59f9daa)",
+          "boot_option_enabled": true
+        },
+        {
+          "boot_option_reference": "Boot0002",
+          "display_name": "opensuse-secureboot",
+          "uefi_device_path": "HD(1,GPT,c5d4f69d-6fc2-48c7-acee-af3f30336...",
+          "boot_option_enabled": true
+        }
+      ]
+    }
 """
-
+from typing import List, Dict
+from jc.jc_types import JSONDictType
 import jc.utils
-import jc.parsers.universal
 
 class info():
     """Provides parser metadata (version, author, etc.)"""
-    version = '0.1'
-    description = '`efibootmgr -v` command parser'
+    version = '1.0'
+    description = '`efibootmgr` command parser'
     author = 'Yaofei Zheng'
-    author_email = ['zyf26256@gmail.com','Yaofei.Zheng@dell.com']
+    author_email = "zyf26256@gmail.com, Yaofei.Zheng@dell.com"
     compatible = ['linux']
     magic_commands = ['efibootmgr']
     tags = ['command']
 
 __version__ = info.version
 
-def _process(proc_data):
-    if 0 == len(proc_data):
-        return proc_data
-    proc_data[0]["timeout_seconds"] = int(proc_data[0]["timeout_seconds"].replace("seconds","").strip(), 10)
-    proc_data[0]["boot_order"] = proc_data[0]["boot_order"].split(',')
-    for boot_opt in proc_data[0]["boot_options"]:
-        boot_opt["boot_option_enabled"] = boot_opt["boot_option_enabled"] == '*'
-    proc_data[0]["mirrored_percentage_above_4g"] = float(proc_data[0]["mirrored_percentage_above_4g"])
-    proc_data[0]["mirror_memory_below_4gb"] = not ("false" == proc_data[0]["mirror_memory_below_4gb"])
+def _process(proc_data: JSONDictType) -> JSONDictType:
+    if 'timeout_seconds' in proc_data:
+        proc_data["timeout_seconds"] = jc.utils.convert_to_int(proc_data["timeout_seconds"])
+
+    if 'boot_order' in proc_data:
+        proc_data["boot_order"] = proc_data["boot_order"].split(',')
+
+    if 'boot_options' in proc_data:
+        for boot_opt in proc_data["boot_options"]:
+            boot_opt["boot_option_enabled"] = bool(boot_opt["boot_option_enabled"] == '*')
+
+    if 'mirrored_percentage_above_4g' in proc_data:
+        proc_data["mirrored_percentage_above_4g"] = float(proc_data["mirrored_percentage_above_4g"])
+
+    if 'mirror_memory_below_4gb' in proc_data:
+        proc_data["mirror_memory_below_4gb"] = not (proc_data["mirror_memory_below_4gb"] == "false")
+
     return proc_data
 
-def parse(data, raw=False, quiet=False):
+
+def parse(
+    data: str,
+    raw: bool = False,
+    quiet: bool = False
+) -> JSONDictType:
+    """
+    Main text parsing function
+
+    Parameters:
+
+        data:        (string)  text data to parse
+        raw:         (boolean) unprocessed output if True
+        quiet:       (boolean) suppress warning messages if True
+
+    Returns:
+
+        List of Dictionaries. Raw or processed structured data.
+    """
     jc.utils.compatibility(__name__, info.compatible, quiet)
     jc.utils.input_type_check(data)
 
-    raw_output = []
+    raw_output: Dict = {}
 
     if jc.utils.has_data(data):
-        boot_opt_list = []
-        efibootmgr_dict = {}
+        boot_opt_list: List = []
+
         for line in filter(None, data.splitlines()):
+
             if "BootCurrent" in line:
-                efibootmgr_dict["boot_current"] = line.split(':')[1].strip()
+                raw_output["boot_current"] = line.split(':')[1].strip()
+
             elif "Timeout" in line:
-                efibootmgr_dict["timeout_seconds"] = line.split(':')[1].strip()
+                raw_output["timeout_seconds"] = line.split(':')[1].strip()
+
             elif "BootOrder" in line:
-                efibootmgr_dict["boot_order"] = line.split(':')[1].strip()
+                raw_output["boot_order"] = line.split(':')[1].strip()
+
             elif "Boot" in line:
-                tmp_dict = {}
+                tmp_dict: Dict = {}
                 boot_record = line.split("\t")
                 tmp_dict["boot_option_reference"] = boot_record[0][0:8]
                 tmp_dict["display_name"] = boot_record[0][10:].strip()
+
                 if len(boot_record) > 1:
                     tmp_dict["uefi_device_path"] = boot_record[1].strip()
+
                 tmp_dict["boot_option_enabled"] = boot_record[0][8]
                 boot_opt_list.append(tmp_dict)
+
             elif "MirroredPercentageAbove4G" in line:
-                efibootmgr_dict["mirrored_percentage_above_4g"] = line.split(':')[1].strip()
+                raw_output["mirrored_percentage_above_4g"] = line.split(':')[1].strip()
+
             elif "MirrorMemoryBelow4GB" in line:
-                efibootmgr_dict["mirror_memory_below_4gb"] = line.split(':')[1].strip()
-            else:
-                # print(line)
-                continue
+                raw_output["mirror_memory_below_4gb"] = line.split(':')[1].strip()
 
-        efibootmgr_dict["boot_options"] = boot_opt_list
+        raw_output["boot_options"] = boot_opt_list
 
-        raw_output.append(efibootmgr_dict)
-
-    if raw:
-        return raw_output
-    else:
-        return _process(raw_output)
+    return raw_output if raw else _process(raw_output)
