a69e55cb1c3b62a8651d476fa6b4720de2c667f7
Kelly Brazil <kellyjonbrazil@gmail.com>
2024-04-27 11:17:37 -0700

    version bump

--- jc/parsers/bluetoothctl.py
+++ jc/parsers/bluetoothctl.py
@@ -108,7 +108,7 @@ import jc.utils
 
 class info():
     """Provides parser metadata (version, author, etc.)"""
-    version = '1.1'
+    version = '1.2'
     description = '`bluetoothctl` command parser'
     author = 'Jake Ob'
     author_email = 'iakopap at gmail.com'
