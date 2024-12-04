4b098bc6f0f6d48e4db096e5220b09fbdacc0a4e
Eden Refael <edenraf@hotmail.com>
2024-12-05 01:05:28 +0200

    added tests for the _process raw=False.

--- tests/test_amixer.py
+++ tests/test_amixer.py
@@ -20,17 +20,26 @@ class AmixerTests(unittest.TestCase):
     def setUp(self):
         self.test_files_out = [f'{file}.out' for file in self.TEST_FILES_NAME]
         self.test_files_json = [f'{file}.json' for file in self.TEST_FILES_NAME]
+        self.test_files_processed_json = [f'{file}-processed.json' for file in self.TEST_FILES_NAME]
 
     def test_amixer_sget(self):
-        for file_out, file_json in zip(self.test_files_out, self.test_files_json):
+        for file_out, file_json, file_processed_json in zip(self.test_files_out, self.test_files_json,
+                                                            self.test_files_processed_json):
             with open(file_out, 'r') as f:
                 amixer_sget_raw_output: str = f.read()
             with open(file_json, 'r') as f:
                 expected_amixer_sget_json_output: str = f.read()
                 expected_amixer_sget_json_map: dict = json.loads(expected_amixer_sget_json_output)
-            amixer_sget_json_map: dict = jc.parse(self.AMIXER_CMD, amixer_sget_raw_output)
-            self.assertEqual(amixer_sget_json_map, expected_amixer_sget_json_map)
+            with open(file_processed_json, 'r') as f:
+                expected_amixer_sget_processed_json_output: str = f.read()
+                expected_amixer_sget_processed_json_map: dict = json.loads(expected_amixer_sget_processed_json_output)
 
+            # Tests for raw=True
+            amixer_sget_json_map: dict = jc.parse(self.AMIXER_CMD, amixer_sget_raw_output, raw=True)
+            self.assertEqual(amixer_sget_json_map, expected_amixer_sget_json_map)
+            # Tests for raw=False process
+            amixer_sget_json_processed_map: dict = jc.parse(self.AMIXER_CMD, amixer_sget_raw_output, raw=False)
+            self.assertEqual(amixer_sget_json_processed_map, expected_amixer_sget_processed_json_map)
 
 
 if __name__ == '__main__':
