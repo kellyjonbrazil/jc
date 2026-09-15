3081b8d18c42abbf7f71fecb878753e05c3708ca
Kelly Brazil <kellyjonbrazil@gmail.com>
2026-09-14 19:11:19 -0700

    doc update

--- jc/parsers/http_headers.py
+++ jc/parsers/http_headers.py
@@ -308,7 +308,7 @@ import jc.utils
 
 class info():
     """Provides parser metadata (version, author, etc.)"""
-    version = '1.0'
+    version = '1.1'
     description = 'HTTP headers parser'
     author = 'Kelly Brazil'
     author_email = 'kellyjonbrazil@gmail.com'
